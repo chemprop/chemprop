8cefac038f6b432d336946105e5d4c8c576c9c1a
Kyle Swanson <swansonk.14@gmail.com>
2018-10-11 13:52:21 -0400

    Drop virtual edges and control split sizes with arg

--- train.py
+++ train.py
@@ -29,11 +29,11 @@ def run_training(args: Namespace) -> float:
 
     logger.debug('Loading data')
     data = get_data(args.data_path, args.dataset_type, num_bins=args.num_bins) 
-    if args.dataset_type == 'regression_with_binning': #note: for now, binning based on whole dataset, not just training set
+    if args.dataset_type == 'regression_with_binning':  # note: for now, binning based on whole dataset, not just training set
         data, bin_predictions, regression_data = data
         args.bin_predictions = bin_predictions
         logger.debug('Splitting data with seed {}'.format(args.seed))
-        train_data, _, _ = split_data(data, seed=args.seed)
+        train_data, _, _ = split_data(data, sizes=args.split_sizes, seed=args.seed)
         _, val_data, test_data = split_data(regression_data, seed=args.seed)
     else:
         logger.debug('Splitting data with seed {}'.format(args.seed))
@@ -41,7 +41,7 @@ def run_training(args: Namespace) -> float:
             train_data, val_data, _ = split_data(data, sizes=(0.8, 0.2, 0.0), seed=args.seed)
             test_data = get_data(args.separate_test_set, args.dataset_type, num_bins=args.num_bins) 
         else:
-            train_data, val_data, test_data = split_data(data, seed=args.seed)
+            train_data, val_data, test_data = split_data(data, sizes=args.split_sizes, seed=args.seed)
     num_tasks = len(data[0][1])
     args.num_tasks = num_tasks
 
