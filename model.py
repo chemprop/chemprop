move gan to own file, implement gan for moe, note there's some other numerical stability bug in moe currently

model.py
@@ -3,7 +3,8 @@ from argparse import Namespace
 import torch.nn as nn
 
 from jtnn import JTNN
-from mpn import MPN, GAN
+from mpn import MPN
+from gan import GAN
 from moe import MOE
 from nn_utils import get_activation_function, initialize_weights
 
@@ -43,6 +44,9 @@ def build_model(args: Namespace) -> nn.Module:
 
     if args.moe:
         model = MOE(args)
+        if args.adversarial:
+            args.output_size = output_size
+            model = GAN(args, prediction_model=model, encoder=model.encoder)
         initialize_weights(model)
 
         return model
@@ -88,7 +92,7 @@ def build_model(args: Namespace) -> nn.Module:
 
     if args.adversarial:
         args.output_size = output_size
-        model = GAN(args, model)
+        model = GAN(args, prediction_model=model, encoder=model[0])
 
     initialize_weights(model)
 

moe.py
@@ -182,6 +182,12 @@ class MOE(nn.Module):
                 batch_encs = self.encoder(train_batch) #bs x hidden
             domain_encs.append(torch.mean(batch_encs, dim=0))
         self.domain_encs = domain_encs
+    
+    def set_domain_encs(self, domain_encs):
+        self.domain_encs = domain_encs
+    
+    def get_domain_encs(self):
+        return self.domain_encs
 
     def compute_loss(self, train_smiles, train_targets, test_smiles):  # TODO(moe) parallelize?
         '''
@@ -248,5 +254,4 @@ class MOE(nn.Module):
         
         #TODO(moe) turn off critic and entropy, tune lambda_moe and m_rank
         loss = (1.0 - self.lambda_moe) * mtl_loss + self.lambda_moe * moe_loss + self.lambda_critic * adv_loss + self.lambda_entropy * entropy_loss
-
         return loss

train_utils.py
@@ -88,7 +88,12 @@ def train(model: nn.Module,
 
     loss_sum, iter_count = 0, 0
     if args.adversarial:
-        train_smiles = data.smiles()
+        if args.moe:
+            train_smiles = []
+            for d in data:
+                train_smiles += d.smiles()
+        else:
+            train_smiles = data.smiles()
         train_val_smiles = train_smiles + val_smiles
         d_loss_sum, g_loss_sum, gp_norm_sum = 0, 0, 0
 
@@ -148,6 +153,9 @@ def train(model: nn.Module,
                 iter_count += len(batch)
 
         loss.backward()
+        import math
+        if math.isnan(compute_gnorm(model)):
+            import pdb; pdb.set_trace()
         if args.max_grad_norm is not None:
             clip_grad_norm_(model.parameters(), args.max_grad_norm)
         optimizer.step()

utils.py
@@ -82,7 +82,7 @@ def save_checkpoint(model: nn.Module, scaler: StandardScaler, args: Namespace, p
         } if scaler is not None else None
     }
     if args.moe:
-        state['domain_encs'] = model.domain_encs
+        state['domain_encs'] = model.get_domain_encs()
     torch.save(state, path)
 
 
@@ -143,9 +143,10 @@ def load_checkpoint(path: str,
     model.load_state_dict(model_state_dict)
 
     if args.moe:
-        model.domain_encs = state['domain_encs']
+        domain_encs = state['domain_encs']
         if args.cuda:
-            model.domain_encs = [encs.cuda() for encs in model.domain_encs]
+            domain_encs = [encs.cuda() for encs in domain_encs]
+        model.set_domain_encs(domain_encs)
 
     if cuda:
         print('Moving model to cuda')