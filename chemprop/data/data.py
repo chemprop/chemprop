Bug fix to bert regarding softmax/cross entropy

chemprop/data/data.py
@@ -102,7 +102,9 @@ class MoleculeDatapoint:
         if not self.bert_pretraining:
             raise Exception('Cannot recreate mask without bert_pretraining on.')
 
-        self.mask = (torch.rand(len(self.targets)) > self.bert_mask_prob).numpy().tolist()  # len = num_atoms  (0s to mask atoms)
+        # 0s to mask atoms which should be predicted
+        # self.mask = [target not in [5, 7] or random.random() > .5 for target in self.targets]
+        self.mask = (torch.rand(len(self.targets)) > self.bert_mask_prob).numpy().tolist()  # len = num_atoms
 
     def set_targets(self, targets):  # for unsupervised pretraining only
         self.targets = targets

chemprop/models/mpn.py
@@ -259,8 +259,7 @@ class MPNEncoder(nn.Module):
         atom_hiddens = self.dropout_layer(atom_hiddens)  # num_atoms x hidden
 
         if self.bert_pretraining:
-            vocab_prob = F.softmax(self.W_v(atom_hiddens), dim=-1)  # num_atoms x vocab_size
-            return vocab_prob[1:]  # leave out atom padding
+            return self.W_v(atom_hiddens)[1:]  # num_atoms x vocab_size (leave out atom padding)
 
         # Readout
         if self.set2set: