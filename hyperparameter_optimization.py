cd1468d4c4aac3e0b160efd070b8ab937412edb6
Kyle Swanson <swansonk.14@gmail.com>
2018-12-21 13:22:01 -0500

    Replacing grid search with bayesian optimization

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,9 +1,8 @@
 from argparse import ArgumentParser, Namespace
 from copy import deepcopy
-import logging
-import os
-import random
-from typing import Dict, List, Union
+from typing import Dict, Union
+
+from hyperopt import fmin, hp, tpe
 
 from chemprop.models import build_model
 from chemprop.nn_utils import param_count
@@ -12,12 +11,11 @@ from chemprop.train import cross_validate
 from model_comparison import create_logger, create_train_logger, DATASETS
 
 
-GRID = {
-    'hidden_size': [300, 600, 1200, 2400],
-    'depth': [3, 4, 5, 6],
-    'dropout': [0.0, 0.2, 0.4],
-    'ffn_num_layers': [1, 2, 3],
-    # 'ffn_hidden_size_multiplier': [0.5, 1.0]
+SPACE = {
+    'hidden_size': hp.choice('hidden_size', [300, 600, 1200, 2400]),
+    'depth': hp.choice('depth', [3, 4, 5, 6]),
+    'dropout': hp.choice('dropout', [0.0, 0.2, 0.4]),
+    'ffn_num_layers': hp.choice('ffn_num_layers', [1, 2, 3])
 }
 
 TRAIN_LOGGER = create_train_logger()
@@ -26,7 +24,7 @@ TRAIN_LOGGER = create_train_logger()
 def grid_search(args: Namespace):
     for dataset_name in args.datasets:
         # Get dataset
-        dataset_type, dataset_path, num_folds, metric = DATASETS[dataset_name]
+        dataset_type, dataset_path, _, metric = DATASETS[dataset_name]
 
         # Create logger for dataset
         logger = create_logger(name=dataset_name, save_dir=args.save_dir, save_name='{}.log'.format(dataset_name))
@@ -36,19 +34,17 @@ def grid_search(args: Namespace):
         dataset_args.data_path = dataset_path
         dataset_args.dataset_type = dataset_type
         dataset_args.save_dir = None
-        dataset_args.num_folds = num_folds
         dataset_args.metric = metric
         modify_train_args(dataset_args)
 
         # Run grid search
         results = []
-        for i in range(args.num_runs_per_dataset):
+
+        # Define hyperparameter optimization
+        def objective(hyperparams: Dict[str, Union[int, float]]) -> float:
             # Copy args
             gs_args = deepcopy(dataset_args)
 
-            # Sample and set hyperparameters
-            random.seed(i)  # need to seed here because cross_validate reseeds with the same seed every time
-            hyperparams = {param: random.choice(options) for param, options in GRID.items()}
             for key, value in hyperparams.items():
                 setattr(gs_args, key, value)
 
@@ -69,9 +65,12 @@ def grid_search(args: Namespace):
                 'num_params': num_params
             })
 
+            return (1 if gs_args.minimize_score else -1) * mean_score
+
+        fmin(objective, SPACE, algo=tpe.suggest, max_evals=args.num_runs_per_dataset)
+
         # Report best result
-        results.sort(key=lambda result: (1 if dataset_args.minimize_score else -1) * result['mean_score'])
-        best_result = results[0]
+        best_result = min(results, key=lambda result: (1 if dataset_args.minimize_score else -1) * result['mean_score'])
         logger.info('best')
         logger.info(best_result['hyperparams'])
         logger.info('num params: {:,}'.format(best_result['num_params']))
@@ -83,10 +82,10 @@ if __name__ == '__main__':
     add_train_args(parser)
     parser.add_argument('--datasets', type=str, nargs='+', default=['all'], choices=DATASETS.keys(),
                         help='Which datasets to perform a grid search on')
-    parser.add_argument('--num_runs_per_dataset', type=int, default=10,
+    parser.add_argument('--num_runs_per_dataset', type=int, default=20,
                         help='Number of hyperparameter choices to try for each datasets')
     args = parser.parse_args()
 
     grid_search(args)
 
-    # python model_comparison.py --data_path blah --dataset_type regression --save_dir logging_dir --datasets all --num_runs_per_dataset 10 --quiet
+    # python hyperparameter_optimization.py --data_path blah --dataset_type regression --save_dir dir --datasets delaney --num_runs_per_dataset 20 --num_folds 3 --quiet
