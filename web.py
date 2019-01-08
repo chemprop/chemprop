dc724af20a18e587f37be27ffa41a3f5f79b9681
Kyle Swanson <swansonk.14@gmail.com>
2019-01-08 14:25:23 -0500

    Option to select gpu on website

--- web.py
+++ web.py
@@ -4,6 +4,7 @@ from tempfile import TemporaryDirectory
 from typing import List
 
 from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+import torch
 from werkzeug.utils import secure_filename
 
 from chemprop.parsing import add_predict_args, add_train_args, modify_train_args
@@ -22,6 +23,8 @@ os.makedirs(app.config['CHECKPOINT_FOLDER'], exist_ok=True)
 app.config['TEMP_FOLDER'] = TEMP_FOLDER.name
 app.config['SMILES_FILENAME'] = 'smiles.csv'
 app.config['PREDICTIONS_FILENAME'] = 'predictions.csv'
+app.config['CUDA'] = torch.cuda.is_available()
+app.config['GPUS'] = list(range(torch.cuda.device_count()))
 
 
 def get_datasets() -> List[str]:
@@ -40,11 +43,15 @@ def home():
 @app.route('/train', methods=['GET', 'POST'])
 def train():
     if request.method == 'GET':
-        return render_template('train.html', datasets=get_datasets())
+        return render_template('train.html',
+                               datasets=get_datasets(),
+                               cuda=app.config['CUDA'],
+                               gpus=app.config['GPUS'])
 
     # Get arguments
     data_name, dataset_type, epochs, checkpoint_name = \
         request.form['dataName'], request.form['datasetType'], int(request.form['epochs']), request.form['checkpointName']
+    gpu = request.form.get('gpu', None)
 
     if not checkpoint_name.endswith('.pt'):
         checkpoint_name += '.pt'
@@ -57,6 +64,11 @@ def train():
     args.data_path = os.path.join(app.config['DATA_FOLDER'], data_name)
     args.dataset_type = dataset_type
     args.epochs = epochs
+    if gpu is not None:
+        if gpu == 'None':
+            args.no_cuda = True
+        else:
+            args.gpu = int(gpu)
 
     with TemporaryDirectory() as temp_dir:
         args.save_dir = temp_dir
@@ -69,19 +81,27 @@ def train():
         os.rename(os.path.join(args.save_dir, 'model_0', 'model.pt'),
                   os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint_name))
 
-    return render_template('train.html', datasets=get_datasets(), trained=True)
+    return render_template('train.html',
+                           datasets=get_datasets(),
+                           cuda=app.config['CUDA'],
+                           gpus=app.config['GPUS'],
+                           trained=True)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'GET':
-        return render_template('predict.html', checkpoints=get_checkpoints())
+        return render_template('predict.html',
+                               checkpoints=get_checkpoints(),
+                               cuda=app.config['CUDA'],
+                               gpus=app.config['GPUS'])
 
     # Get arguments
     smiles, checkpoint_name = request.form['smiles'], request.form['checkpointName']
     smiles = smiles.split()
     checkpoint_path = os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint_name)
     task_names = load_task_names(checkpoint_path)
+    gpu = request.form.get('gpu', None)
 
     # Create and modify args
     parser = ArgumentParser()
@@ -91,13 +111,19 @@ def predict():
     preds_path = os.path.join(app.config['TEMP_FOLDER'], app.config['PREDICTIONS_FILENAME'])
     args.preds_path = preds_path
     args.checkpoint_paths = [checkpoint_path]
-    # args.gpu = TODO
+    if gpu is not None:
+        if gpu == 'None':
+            args.no_cuda = True
+        else:
+            args.gpu = int(gpu)
 
     # Run prediction
     preds = make_predictions(args, smiles=smiles)
 
     return render_template('predict.html',
                            checkpoints=get_checkpoints(),
+                           cuda=app.config['CUDA'],
+                           gpus=app.config['GPUS'],
                            predicted=True,
                            smiles=smiles,
                            num_smiles=len(smiles),
