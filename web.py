ce033d10874fd3a36dd08e8dc40789d3173533d5
Kyle Swanson <swansonk.14@gmail.com>
2019-01-08 03:20:25 -0500

    Website now maintains a store of data and model checkpoints

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ import os
 from tempfile import TemporaryDirectory
 from typing import List
 
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 import torch
 from werkzeug.utils import secure_filename
 
@@ -13,18 +13,14 @@ from chemprop.train.run_training import run_training
 from chemprop.utils import load_args, load_checkpoint, load_scalers
 
 
-UPLOAD_FOLDER = TemporaryDirectory()
-DOWNLOAD_FOLDER = TemporaryDirectory()
-PREDICTIONS_FILENAME = 'predictions.csv'
-MODEL_FILENAME = 'model.pt'
-
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER.name
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER.name
-app.config['PREDICTIONS_FILENAME'] = PREDICTIONS_FILENAME
-app.config['MODEL_FOLDER'] = os.path.join(app.config['DOWNLOAD_FOLDER'], 'model_0')
-app.config['MODEL_FILENAME'] = MODEL_FILENAME
+app.config['DATA_FOLDER'] = 'web_data'
+os.makedirs(app.config['DATA_FOLDER'], exist_ok=True)
+app.config['CHECKPOINT_FOLDER'] = 'web_checkpoints'
+os.makedirs(app.config['CHECKPOINT_FOLDER'], exist_ok=True)
+app.config['PREDICTIONS_FOLDER'] = 'web_predictions'
+os.makedirs(app.config['PREDICTIONS_FOLDER'], exist_ok=True)
+app.config['PREDICTIONS_FILENAME'] = 'predictions.csv'
 
 
 def make_predictions(checkpoint_path: str, smiles: List[str]) -> List[List[float]]:
@@ -71,6 +67,14 @@ def get_task_names(checkpoint_path: str) -> List[str]:
     return args.task_names
 
 
+def get_datasets() -> List[str]:
+    return os.listdir(app.config['DATA_FOLDER'])
+
+
+def get_checkpoints() -> List[str]:
+    return os.listdir(app.config['CHECKPOINT_FOLDER'])
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -79,64 +83,58 @@ def home():
 @app.route('/train', methods=['GET', 'POST'])
 def train():
     if request.method == 'GET':
-        return render_template('train.html')
+        return render_template('train.html', datasets=get_datasets())
 
-    # Get dataset type and epochs
-    dataset_type, epochs = request.form['datasetType'], int(request.form['epochs'])
+    # Get arguments
+    data_name, dataset_type, epochs, checkpoint_name = \
+        request.form['dataName'], request.form['datasetType'], int(request.form['epochs']), request.form['checkpointName']
 
-    # Upload data file
-    data = request.files['data']
-    data_name = secure_filename(data.filename)
-    data_path = os.path.join(app.config['UPLOAD_FOLDER'], data_name)
-    data.save(data_path)
+    if not checkpoint_name.endswith('.pt'):
+        checkpoint_name += '.pt'
 
     # Create and modify args
     parser = ArgumentParser()
     add_train_args(parser)
     args = parser.parse_args()
 
-    args.data_path = data_path
+    args.data_path = os.path.join(app.config['DATA_FOLDER'], data_name)
     args.dataset_type = dataset_type
-    args.save_dir = app.config['DOWNLOAD_FOLDER']
     args.epochs = epochs
 
-    modify_train_args(args)
+    with TemporaryDirectory() as temp_dir:
+        args.save_dir = temp_dir
+        modify_train_args(args)
 
-    # Run training
-    run_training(args)
+        # Run training
+        run_training(args)
 
-    return render_template('train.html', trained=True)
+        # Move checkpoint
+        os.rename(os.path.join(args.save_dir, 'model_0', 'model.pt'),
+                  os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint_name))
 
-
-@app.route('/download_model')
-def download_model():
-    return send_from_directory(app.config['MODEL_FOLDER'], app.config['MODEL_FILENAME'], as_attachment=True)
+    return render_template('train.html', datasets=get_datasets(), trained=True)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'GET':
-        return render_template('predict.html')
+        return render_template('predict.html', checkpoints=get_checkpoints())
 
-    # Get smiles
-    smiles = request.form['smiles']
+    # Get smiles and checkpoint path
+    smiles, checkpoint_name = request.form['smiles'], request.form['checkpointName']
     smiles = smiles.split()
-
-    # Upload model checkpoint
-    checkpoint = request.files['checkpoint']
-    checkpoint_name = secure_filename(checkpoint.filename)
-    checkpoint_path = os.path.join(app.config['UPLOAD_FOLDER'], checkpoint_name)
-    checkpoint.save(checkpoint_path)
+    checkpoint_path = os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint_name)
 
     # Run prediction
     task_names = get_task_names(checkpoint_path)
     preds = make_predictions(checkpoint_path, smiles)
 
     # Save preds
-    preds_path = os.path.join(app.config['DOWNLOAD_FOLDER'], app.config['PREDICTIONS_FILENAME'])
+    preds_path = os.path.join(app.config['PREDICTIONS_FOLDER'], app.config['PREDICTIONS_FILENAME'])
     save_predictions(preds_path, task_names, smiles, preds)
 
     return render_template('predict.html',
+                           checkpoints=get_checkpoints(),
                            predicted=True,
                            smiles=smiles,
                            num_smiles=len(smiles),
@@ -147,7 +145,57 @@ def predict():
 
 @app.route('/download_predictions')
 def download_predictions():
-    return send_from_directory(app.config['DOWNLOAD_FOLDER'], app.config['PREDICTIONS_FILENAME'], as_attachment=True)
+    return send_from_directory(app.config['PREDICTIONS_FOLDER'], app.config['PREDICTIONS_FILENAME'], as_attachment=True)
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'GET':
+        return render_template('data.html', datasets=get_datasets())
+
+    # Upload data file
+    data = request.files['data']
+    data_name = secure_filename(data.filename)
+    data_path = os.path.join(app.config['DATA_FOLDER'], data_name)
+    data.save(data_path)
+
+    return render_template('data.html', datasets=get_datasets())
+
+
+@app.route('/data/download/<string:dataset>')
+def download_data(dataset: str):
+    return send_from_directory(app.config['DATA_FOLDER'], dataset, as_attachment=True)
+
+
+@app.route('/data/delete/<string:dataset>')
+def delete_data(dataset: str):
+    os.remove(os.path.join(app.config['DATA_FOLDER'], dataset))
+    return redirect(url_for('data'))
+
+
+@app.route('/checkpoints', methods=['GET', 'POST'])
+def checkpoints():
+    if request.method == 'GET':
+        return render_template('checkpoints.html', checkpoints=get_checkpoints())
+
+    # Upload checkpoint file
+    checkpoint = request.files['data']
+    checkpoint_name = secure_filename(checkpoint.filename)
+    checkpoint_path = os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint_name)
+    checkpoint.save(checkpoint_path)
+
+    return render_template('checkpoints.html', checkpoints=get_checkpoints())
+
+
+@app.route('/checkpoints/download/<string:checkpoint>')
+def download_checkpoint(checkpoint: str):
+    return send_from_directory(app.config['CHECKPOINT_FOLDER'], checkpoint, as_attachment=True)
+
+
+@app.route('/checkpoints/delete/<string:checkpoint>')
+def delete_checkpoint(checkpoint: str):
+    os.remove(os.path.join(app.config['CHECKPOINT_FOLDER'], checkpoint))
+    return redirect(url_for('checkpoints'))
 
 
 if __name__ == "__main__":
