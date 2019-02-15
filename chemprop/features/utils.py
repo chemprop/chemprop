e082a16562fef9c890b0837c71753a5caf905a8b
Kyle Swanson <swansonk.14@gmail.com>
2019-02-15 18:03:55 -0500

    Changing features saving and loading to default to compressed npz

--- chemprop/features/utils.py
+++ chemprop/features/utils.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import pickle
 from typing import List
@@ -5,33 +6,48 @@ from typing import List
 import numpy as np
 
 
-# TODO: save_features
-
-
-def load_features(path: str) -> List[np.ndarray]:
+def save_features(path: str, features: List[np.ndarray]):
     """
-    Loads features saved as a .pckl file or as a directory of .pckl files.
+    Saves features to a compressed .npz file with array name "features".
 
-    If path is a directory, assumes features are saved in files named 0.pckl, 1.pckl, ...
-
-    :param path: Path to a .pckl file or a directory of .pckl files named as above.
-    :return: A list of numpy arrays containing the features.
+    :param path: Path to a .npz file where the features will be saved.
+    :param features: A list of 1D numpy arrays containing the features for molecules.
     """
-    if os.path.isfile(path):
+    np.savez_compressed(path, features)
+
+
+def load_features(path: str) -> np.ndarray:
+    """
+    Loads features saved in a variety of formats.
+
+    Supported formats:
+    - .npz compressed (assumes features are saved with name "features")
+    - .npz (assumes features are saved with name "features")
+    - .npy
+    - .csv (assumes comma-separated features with a header and with one line per molecule)
+    - .pckl containing a sparse numpy array (TODO: remove this option once we are no longer dependent on it)
+
+    All formats assume that the SMILES strings loaded elsewhere in the code are in the same
+    order as the features loaded here.
+
+    :param path: Path to a file containing features.
+    :return: A 2D numpy array of size (num_molecules, features_size) containing the features.
+    """
+    extension = os.path.splitext(path)[1]
+
+    if extension == '.npz':
+        features = np.load(path)['features']
+    elif extension == '.npy':
+        features = np.load(path)
+    elif extension == '.csv':
+        with open(path) as f:
+            reader = csv.reader(f)
+            next(reader)  # skip header
+            features = np.array([[float(value) for value in row] for row in reader])
+    elif extension == '.pckl':
         with open(path, 'rb') as f:
-            features = pickle.load(f)
-        features = [np.squeeze(np.array(feat.todense())) for feat in features]
+            features = np.array([np.squeeze(np.array(feat.todense())) for feat in pickle.load(f)])
     else:
-        features = []
-        features_num = 0
-        features_path = os.path.join(path, f'{features_num}.pckl')
-
-        while os.path.exists(features_path):
-            with open(features_path, 'rb') as f:
-                feats = pickle.load(f)
-            features.extend([np.squeeze(np.array(feat.todense())) for feat in feats])
-
-            features_num += 1
-            features_path = os.path.join(path, f'{features_num}.pckl')
+        raise ValueError(f'Features path extension {extension} not supported.')
 
     return features
