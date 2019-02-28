8b42281070661b2ea084bb1948fcaf2fd4bce66d
lhirschfeld <liortulip@gmail.com>
2019-02-28 13:42:22 -0500

    Fixed TEMP_FOLDER.

--- web/config.py
+++ web/config.py
@@ -8,11 +8,13 @@ from tempfile import TemporaryDirectory
 
 import torch
 
+_TEMP_FOLDER_OBJECT = TemporaryDirectory()
+
 DEFAULT_USER_ID = 1
 ROOT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 DATA_FOLDER = os.path.join(ROOT_FOLDER, 'app/web_data')
 CHECKPOINT_FOLDER = os.path.join(ROOT_FOLDER, 'app/web_checkpoints')
-TEMP_FOLDER = os.path.join(ROOT_FOLDER, TemporaryDirectory().name)
+TEMP_FOLDER = os.path.join(ROOT_FOLDER, _TEMP_FOLDER_OBJECT.name)
 SMILES_FILENAME = 'smiles.csv'
 PREDICTIONS_FILENAME = 'predictions.csv'
 DB_FILENAME = 'chemprop.sqlite3'
