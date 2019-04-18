398e566313709e9a1a10614a8cd932a7ecf650b1
Kevin Yang <yangk@31-39-59.wireless.csail.mit.edu>
2019-04-18 18:35:04 -0400

    code to dump corresponding random and scaffold indices for sliding window time indices; modify format as necessary later

--- scripts/create_crossval_splits.py
+++ scripts/create_crossval_splits.py
@@ -2,30 +2,50 @@ from argparse import ArgumentParser
 import pickle
 import os
 import random
+from copy import deepcopy
 
 import numpy as np
 
 from chemprop.data.utils import get_data
 
+def split_indices(all_indices, scaffold=False, data=None, shuffle=True):
+    if scaffold:
+        raise NotImplementedError  # TODO
+    else: # random
+        if shuffle:
+            random.shuffle(all_indices)
+        fold_indices = []
+        for i in range(args.num_folds):
+            begin, end = int(i * num_data / args.num_folds), int((i+1) * num_data / args.num_folds)
+            fold_indices.append(np.array(all_indices[begin:end]))
+    return fold_indices
+
+def create_time_splits(args):
+    #ASSUME DATA GIVEN IN CHRONOLOGICAL ORDER. 
+    # this will dump a very different format of indices, with all in one file; TODO modify as convenient later.
+    data = get_data(args.data_path)
+    num_data = len(data)
+    all_indices = list(range(num_data))
+    fold_indices = []
+    for i in range(args.num_folds - args.time_folds_per_train_set - 1):
+        begin, end = int(i * num_data / args.num_folds), int((i + args.time_folds_per_train_set + 2) * num_data / args.num_folds)
+        subset_indices = all_indices[begin:end]
+        subset_data = data[begin:end] # TODO check this syntax?
+        fold_indices.append({'random': split_indices(deepcopy(subset_indices)), # so we don't shuffle this for time split later; TODO check this is all correct
+                             'scaffold': split_indices(subset_indices, scaffold=True, data=subset_data)
+                             'time': split_indices(subset_data, shuffle=False)})
+    with open(args.save_dir, 'wb') as wf:
+        pickle.dump(fold_indices, wf)
 
 def create_crossval_splits(args):
     data = get_data(args.data_path)
     num_data = len(data)
     if args.split_type == 'random':
         all_indices = list(range(num_data))
-        random.shuffle(all_indices)
-        fold_indices = []
-        for i in range(args.num_folds):
-            begin, end = int(i * num_data / args.num_folds), int((i+1) * num_data / args.num_folds)
-            fold_indices.append(np.array(all_indices[begin:end]))
+        fold_indices = split_indices(all_indices, scaffold=False)
     elif args.split_type == 'scaffold':
-        raise NotImplementedError  # TODO
-    elif args.split_type == 'time': # same as random, but without shuffling. ASSUME DATA GIVEN IN CHRONOLOGICAL ORDER. 
         all_indices = list(range(num_data))
-        fold_indices = []
-        for i in range(args.num_folds):
-            begin, end = int(i * num_data / args.num_folds), int((i+1) * num_data / args.num_folds)
-            fold_indices.append(np.array(all_indices[begin:end]))
+        fold_indices = split_indices(all_indices, scaffold=True, data=data)
     else:
         raise ValueError
     os.makedirs(os.path.join(args.save_dir, args.split_type), exist_ok=True)
@@ -40,10 +60,15 @@ if __name__ == '__main__':
                         help='Path to CSV file with dataset of molecules')
     parser.add_argument('--save_dir', type=str, required=True,
                         help='Path to CSV file where splits will be saved')
-    parser.add_argument('--split_type', type=str, choices=['random', 'scaffold', 'time'], required=True,
+    parser.add_argument('--split_type', type=str, choices=['random', 'scaffold', 'time_window'], required=True,
                         help='Random or scaffold based split')
     parser.add_argument('--num_folds', type=int, default=10,
                         help='Number of cross validation folds')
+    parser.add_argument('--time_folds_per_train_set', type=int, default=3,
+                        help='X:1:1 train:val:test for time split sliding window')
     args = parser.parse_args()
-
-    create_crossval_splits(args)
+    
+    if args.split_type == 'time_window':
+        create_time_splits(args)
+    else:
+        create_crossval_splits(args)
