3dcdf9daee7a0ad83ce2ce093cdb788db76a6773
Kevin Yang <yangk@31-39-59.wireless.csail.mit.edu>
2019-04-18 13:02:14 -0400

    few more comments

--- hyperopt.sh
+++ hyperopt.sh
@@ -5,6 +5,10 @@ datasets=('delaney' 'lipo' 'freesolv' 'pdbbind_full' 'pdbbind_core' 'pdbbind_ref
 dataset_type=('regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'classification' 'classification' 'classification' 'classification' 'classification' 'classification' 'regression' 'classification' 'classification' 'classification' 'classification')
 metrics=('rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'mae' 'mae' 'auc' 'auc' 'auc' 'auc' 'auc' 'auc' 'mae' 'prc-auc' 'prc-auc' 'auc' 'auc')
 sizes=('small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'small' 'big' 'big' 'big' 'small' 'big')
+# datasets=('delaney')
+# dataset_type=('regression')
+# metrics=('rmse')
+# sizes=('small')
 
 folds=(0 1 2 3 4 5 6 7 8 9)
 gpu=0
@@ -16,7 +20,7 @@ for i in ${!datasets[@]}; do
         echo ${folds[$fold]}
         file=./crossval_index_files/${sizes[$i]}/${folds[$fold]}.pkl
         if [ ! -e "$file" ]; then
-            echo "Fold indices do not exist"
+            echo "Fold indices do not exist" # you should expect this to happen when not testing on all 10 folds
         else 
             CUDA_VISIBLE_DEVICES=$gpu python hyperparameter_optimization.py --data_path data/${datasets[$i]}.csv --dataset_type ${dataset_type[$i]} --split_type crossval --crossval_index_file crossval_index_files/${sizes[$i]}/${folds[$fold]}.pkl --crossval_index_dir crossval_folds/${datasets[$i]}/random --features_path /data/rsg/chemistry/yangk/saved_features/${datasets[$i]}.pckl --no_features_scaling --num_iters 30 --config_save_path ../ckpt/417_hyperopt/${datasets[$i]}/random/${folds[$fold]}/config.json --log_dir ../ckpt/417_hyperopt/${datasets[$i]}/random/${folds[$fold]}/logdir --quiet --metric ${metrics[$i]} &
         fi 
