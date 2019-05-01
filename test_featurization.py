86d065ce1700232b66008d60768831b993400c9f
woodyn <@#nate1012>
2019-05-01 15:21:24 -0400

    edited struct

--- test_featurization.py
+++ test_featurization.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
     args.atom_messages=False
-    m = MolGraph('C/C=C/C(=O)N1CCC(CC1)CC(=O)N2CC[C@@H](C2)Nc3ccc4cc(ccc4n3)OC', args)
+    m = MolGraph('CC',args)
     print(m.f_atoms)
     print(m.f_bonds)
