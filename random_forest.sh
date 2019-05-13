0a8af662b8b6a49706402dcdfc8aaca55eb66aa6
Kyle Swanson <swansonk.14@gmail.com>
2019-05-13 18:03:44 -0400

    Special case for tox21 random forest

--- random_forest.sh
+++ random_forest.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -x
 
-# TODO do pcba, chembl separately later because they're bigger
-datasets=('delaney' 'lipo' 'freesolv' 'pdbbind_full' 'pdbbind_core' 'pdbbind_refined' 'qm7' 'qm8' 'bace' 'bbbp' 'sider' 'clintox' 'tox21' 'toxcast' 'hiv' 'muv' 'qm9') # 'pcba' 'chembl')
-dataset_type=('regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'classification' 'classification' 'classification' 'classification' 'classification' 'classification' 'classification' 'classification' 'regression') # 'classification' 'classification')
-metrics=('rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'mae' 'mae' 'auc' 'auc' 'auc' 'auc' 'auc' 'auc' 'auc' 'prc-auc' 'mae') # 'auc' 'auc')
-sizes=('one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'big' 'big') # 'big' 'big')
+datasets=('delaney' 'lipo' 'freesolv' 'pdbbind_full' 'pdbbind_core' 'pdbbind_refined' 'qm7' 'qm8' 'bace' 'bbbp' 'sider' 'clintox' 'hiv' 'qm9' 'tox21')
+dataset_type=('regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'regression' 'classification' 'classification' 'classification' 'classification' 'classification' 'regression' 'classification')
+metrics=('rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'rmse' 'mae' 'mae' 'auc' 'auc' 'auc' 'auc' 'auc' 'mae' 'auc')
+sizes=('one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'big' 'one')
 # datasets=('delaney')
 # dataset_type=('regression')
 # metrics=('rmse')
@@ -26,10 +25,15 @@ for i in ${!datasets[@]}; do
             file="/data/rsg/chemistry/yangk/lsc_experiments_dump_splits/data/${datasets[$i]}/scaffold/fold_$fold/0/split_indices.pckl"
             split_info="--split_type predetermined --folds_file $file --val_fold_index 1 --test_fold_index 2"
         fi
+        if [[ "${datasets[$i]}" == "tox21" ]]; then
+            single_task="--single_task"
+        else
+            single_task=""
+        fi
         if [[ ! -e "$file" ]]; then
             echo "Fold indices do not exist" # you should expect this to happen when not testing on all 10 folds
         else
-            python random_forest.py --data_path data/${datasets[$i]}.csv --dataset_type ${dataset_type[$i]} --save_dir ../ckpt/417_random_forest/${datasets[$i]}/${split_type}/${folds[$fold]} ${split_info} --quiet --metric ${metrics[$i]}
+            python random_forest.py ${single_task} --data_path data/${datasets[$i]}.csv --dataset_type ${dataset_type[$i]} --save_dir ../ckpt/417_random_forest/${datasets[$i]}/${split_type}/${folds[$fold]} ${split_info} --quiet --metric ${metrics[$i]}
         fi
     done
 done
