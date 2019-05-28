025e8eca83f8b11953c3057a56532fa035b1fda5
Kevin Yang <yangk@Kevins-MacBook-Pro-6.local>
2019-05-28 14:26:30 -0400

    clean up experiment scripts to prepare merge with master

--- create_splits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-split_type=$1;  # random or scaffold
-datasets=('delaney' 'lipo' 'freesolv' 'pdbbind_full' 'pdbbind_core' 'pdbbind_refined' 'qm7' 'qm8' 'qm9' 'bace' 'bbbp' 'sider' 'clintox' 'tox21' 'toxcast' 'muv' 'hiv' 'pcba' 'chembl')
-
-for i in ${!datasets[@]}; do
-    echo ${datasets[$i]}
-    python scripts/create_crossval_splits.py --data_path data/${datasets[$i]}.csv --save_dir crossval_folds/${datasets[$i]} --split_type ${split_type}
-done
