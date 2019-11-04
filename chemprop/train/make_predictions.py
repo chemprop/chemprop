acee64f4c23b3cbc3e7c5da0a87e35b0a465a2a1
Allison Tam <actam@mit.edu>
2019-11-04 02:10:37 -0800

    fixes preds for molpairdataset

--- chemprop/train/make_predictions.py
+++ chemprop/train/make_predictions.py
@@ -7,7 +7,7 @@ import torch
 from tqdm import tqdm
 
 from .predict import predict
-from chemprop.data import MoleculeDataset
+from chemprop.data import MolPairDataset
 from chemprop.data.utils import get_data, get_data_from_smiles
 from chemprop.utils import load_args, load_checkpoint, load_scalers
 
@@ -39,9 +39,9 @@ def make_predictions(args: Namespace, smiles: List[str] = None) -> List[Optional
         test_data = get_data(path=args.test_path, args=args, use_compound_names=args.use_compound_names, skip_invalid_smiles=False)
 
     print('Validating SMILES')
-    valid_indices = [i for i in range(len(test_data)) if test_data[i].mol is not None]
+    valid_indices = [i for i in range(len(test_data)) if test_data[i].mol1 is not None]
     full_data = test_data
-    test_data = MoleculeDataset([test_data[i] for i in valid_indices])
+    test_data = MolPairDataset([test_data[i] for i in valid_indices])
 
     # Edge case if empty list of smiles is provided
     if len(test_data) == 0:
