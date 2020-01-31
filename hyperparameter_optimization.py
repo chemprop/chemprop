b23114dd0eea76b61870095444c82b014debd543
Allison Tam <actam@rosetta10.csail.mit.edu>
2020-01-31 16:09:27 -0500

    auto configs for hyperparam

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 import json
 from typing import Dict, Union
 import os
+import time
 
 from hyperopt import fmin, hp, tpe
 import numpy as np
@@ -97,12 +98,17 @@ if __name__ == '__main__':
     add_train_args(parser)
     parser.add_argument('--num_iters', type=int, default=20,
                         help='Number of hyperparameter choices to try')
-    parser.add_argument('--config_save_path', type=str,
-                        help='Path to .json file where best hyperparameter settings will be written')
-    parser.add_argument('--log_dir', type=str,
-                        help='(Optional) Path to a directory where all results of the hyperparameter optimization will be written')
+    # parser.add_argument('--config_save_path', type=str,
+                        # help='Path to .json file where best hyperparameter settings will be written')
+    # parser.add_argument('--log_dir', type=str,
+                        # help='(Optional) Path to a directory where all results of the hyperparameter optimization will be written')
     args = parser.parse_args()
     modify_train_args(args)
+
+    args.config_save_path = os.path.join(args.save_dir, 'hyper.json')
+    args.log_dir = args.save_dir
     assert args.config_save_path  # check if supplied
 
+    start = time.time()
     grid_search(args)
+    print('Execution time:', (time.time()-start)/3600, 'hrs')
--- train.py
+++ train.py
@@ -3,9 +3,12 @@
 from chemprop.parsing import parse_train_args
 from chemprop.train import cross_validate
 from chemprop.utils import create_logger
+import time
 
 
 if __name__ == '__main__':
     args = parse_train_args()
     logger = create_logger(name='train', save_dir=args.save_dir, quiet=args.quiet)
+    start = time.time()
     cross_validate(args, logger)
+    print('Elapsed training time:', (time.time()-start)/3600, 'hrs')
