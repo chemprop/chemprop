a73d02d42f4c1f6e133f0740e9b6da95c93760a8
Allison Tam <actam@rosetta10.csail.mit.edu>
2020-03-05 15:00:13 -0500

    cleaning up following revert.

--- chemprop/data/__init__.py
+++ chemprop/data/__init__.py
@@ -1,4 +1,3 @@
 from .data import MolPairDatapoint, MolPairDataset
-# from .singleData_old import MoleculeDatapoint, MoleculeDataset
 from .scaffold import scaffold_to_smiles
 from .scaler import StandardScaler
