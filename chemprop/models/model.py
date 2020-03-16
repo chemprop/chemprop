adds ability to use contrastive loss w/ configs. also fixes(?) logging scaling for training loss

chemprop/models/model.py
@@ -10,21 +10,17 @@ from chemprop.nn_utils import get_activation_function, initialize_weights
 class MoleculeModel(nn.Module):
     """A MoleculeModel is a model which contains a message passing network following by feed-forward layers."""
 
-    def __init__(self, classification: bool, multiclass: bool):
+    def __init__(self, output_raw: bool):
         """
         Initializes the MoleculeModel.
 
-        :param classification: Whether the model is a classification model.
+        :param raw_score: Whether the model should apply activation to output.
         """
         super(MoleculeModel, self).__init__()
 
-        self.classification = classification
-        if self.classification:
-            self.sigmoid = nn.Sigmoid()
-        self.multiclass = multiclass
-        if self.multiclass:
-            self.multiclass_softmax = nn.Softmax(dim=2)
-        assert not (self.classification and self.multiclass)
+        self.activation = nn.Identity()
+        if output_raw:
+            self.activation = nn.Sigmoid()
 
     def create_encoder(self, args: Namespace):
         """
@@ -117,12 +113,8 @@ class MoleculeModel(nn.Module):
         output = self.ffn(newInput)
 
         # Don't apply sigmoid during training b/c using BCEWithLogitsLoss
-        if self.classification and not self.training:
-            output = self.sigmoid(output)
-        if self.multiclass:
-            output = output.reshape((output.size(0), -1, self.num_classes)) # batch size x num targets x num classes per target
-            if not self.training:
-                output = self.multiclass_softmax(output) # to get probabilities during evaluation, but not during training as we're using CrossEntropyLoss
+        if not self.training:
+            output = self.activation(output)
 
         return output
 
@@ -137,9 +129,9 @@ def build_model(args: Namespace) -> nn.Module:
     output_size = args.num_tasks
     args.output_size = output_size
     if args.dataset_type == 'multiclass':
-        args.output_size *= args.multiclass_num_classes
+        raise NotImplementedError
 
-    model = MoleculeModel(classification=args.dataset_type == 'classification', multiclass=args.dataset_type == 'multiclass')
+    model = MoleculeModel(output_raw=args.output_raw)
     model.create_encoder(args)
     model.create_ffn(args)
 

chemprop/train/evaluate.py
@@ -4,8 +4,9 @@ from typing import Callable, List, Union
 import torch
 import torch.nn as nn
 
+from .loss_funcs import ContrastiveLoss
 from .predict import predict
-from chemprop.data import MolPairDataset, StandardScaler
+from chemprop.data import  convert2contrast, MolPairDataset, StandardScaler
 
 
 def val_loss(model: nn.Module,
@@ -24,14 +25,19 @@ def val_loss(model: nn.Module,
     """
     model.train()
     data.shuffle()
+    if type(loss_func) == ContrastiveLoss:
+        data = convert2contrast(data)
     loss_sum, total_num = 0, 0
 
     for i in range(0, len(data), batch_size):
         mol_batch = MolPairDataset(data[i:i + batch_size])
         smiles_batch, features_batch, target_batch = mol_batch.smiles(), mol_batch.features(), mol_batch.targets()
         batch = smiles_batch
-        mask = torch.Tensor([[x is not None for x in tb] for tb in target_batch])
         targets = torch.Tensor([[0 if x is None else x for x in tb] for tb in target_batch])
+        if type(loss_func) == ContrastiveLoss:
+            mask = targets
+        else:
+            mask = torch.Tensor([[x is not None for x in tb] for tb in target_batch])
 
         if next(model.parameters()).is_cuda:
             mask, targets = mask.cuda(), targets.cuda()

chemprop/train/train.py
@@ -1,6 +1,6 @@
 from argparse import Namespace
 import logging
-from typing import Callable, List, Union
+from typing import Callable, List
 
 from tensorboardX import SummaryWriter
 import torch
@@ -9,12 +9,12 @@ from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler
 from tqdm import trange
 
-from chemprop.data import MolPairDataset
+from chemprop.data import MolPairDataset, convert2contrast
 from chemprop.nn_utils import compute_gnorm, compute_pnorm, NoamLR
 
 
 def train(model: nn.Module,
-          data: Union[MolPairDataset, List[MolPairDataset]],
+          data: MolPairDataset,
           loss_func: Callable,
           optimizer: Optimizer,
           scheduler: _LRScheduler,
@@ -37,13 +37,15 @@ def train(model: nn.Module,
     :return: The total number of iterations (training examples) trained on so far.
     """
     debug = logger.debug if logger is not None else print
-    
+
     model.train()
-    
-    data.shuffle()
+
+    data.shuffle()  # Very important this is done before conversion to maintain randomness in contrastive dataset.
 
     loss_sum, iter_count = 0, 0
 
+    if args.loss_func == 'contrastive':
+        data = convert2contrast(data)
     num_iters = len(data) // args.batch_size * args.batch_size  # don't use the last batch if it's small, for stability
 
     iter_size = args.batch_size
@@ -55,8 +57,11 @@ def train(model: nn.Module,
         mol_batch = MolPairDataset(data[i:i + args.batch_size])
         smiles_batch, features_batch, target_batch = mol_batch.smiles(), mol_batch.features(), mol_batch.targets()
         batch = smiles_batch
-        mask = torch.Tensor([[x is not None for x in tb] for tb in target_batch])
         targets = torch.Tensor([[0 if x is None else x for x in tb] for tb in target_batch])
+        if args.loss_func == 'contrastive':
+            mask = targets
+        else:
+            mask = torch.Tensor([[x is not None for x in tb] for tb in target_batch])
 
         if next(model.parameters()).is_cuda:
             mask, targets = mask.cuda(), targets.cuda()
@@ -78,7 +83,7 @@ def train(model: nn.Module,
         loss = loss.sum() / mask.sum()
 
         loss_sum += loss.item()
-        iter_count += len(mol_batch)
+        iter_count += 1
 
         loss.backward()
         if args.grad_clip:
@@ -88,7 +93,7 @@ def train(model: nn.Module,
         if isinstance(scheduler, NoamLR):
             scheduler.step()
 
-        n_iter += len(mol_batch)
+        n_iter += args.batch_size
 
         # Log and/or add to tensorboard
         if (n_iter // args.batch_size) % args.log_frequency == 0:

chemprop/utils.py
@@ -14,6 +14,7 @@ from torch.optim.lr_scheduler import _LRScheduler
 from chemprop.data import StandardScaler
 from chemprop.models import build_model, MoleculeModel
 from chemprop.nn_utils import NoamLR
+from chemprop.train.loss_funcs import ContrastiveLoss
 
 
 def makedirs(path: str, isfile: bool = False):
@@ -170,16 +171,28 @@ def get_loss_func(args: Namespace) -> nn.Module:
     :param args: Namespace containing the dataset type ("classification" or "regression").
     :return: A PyTorch loss function.
     """
-    if args.dataset_type == 'classification':
-        return nn.BCEWithLogitsLoss(reduction='none')
+    if args.loss_func == 'default':
+        if args.dataset_type == 'classification':
+            return nn.BCEWithLogitsLoss(reduction='none')
+        elif args.dataset_type == 'regression':
+            return nn.MSELoss(reduction='none')
+        elif args.dataset_type == 'multiclass':
+            return nn.CrossEntropyLoss(reduction='none')
+        else:
+            raise ValueError(f'Dataset type "{args.dataset_type}" not supported.')
+    elif args.loss_func == 'contrastive':
+        return ContrastiveLoss()
 
-    if args.dataset_type == 'regression':
-        return nn.MSELoss(reduction='none')
-    
-    if args.dataset_type == 'multiclass':
-        return nn.CrossEntropyLoss(reduction='none')
 
-    raise ValueError(f'Dataset type "{args.dataset_type}" not supported.')
+def output_raw(args: Namespace) -> bool:
+    """
+    Gets if model should return raw score based on dataset type and loss func settings.
+
+    :param args: Namespace.
+    :return: If model should return score before (True) or after activation.
+    """
+    loss_type = type(get_loss_func(args))
+    return loss_type == nn.BCEWithLogitsLoss or loss_type == nn.CrossEntropyLoss
 
 
 def prc_auc(targets: List[int], preds: List[float]) -> float: