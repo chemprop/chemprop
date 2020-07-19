18a1529c5a3edd6cabd040a530fec4fed4f19d40
Kyle Swanson <swansonk.14@gmail.com>
2020-07-19 01:48:19 -0400

    Complete documentation of arguments

--- chemprop/args.py
+++ chemprop/args.py
@@ -57,18 +57,30 @@ def get_checkpoint_paths(checkpoint_path: Optional[str] = None,
 class CommonArgs(Tap):
     """:class:`CommonArgs` contains arguments that are used in both :class:`TrainArgs` and :class:`PredictArgs`."""
 
-    smiles_column: str = None  # Name of the column containing SMILES strings. By default, uses the first column.
-    checkpoint_dir: str = None  # Directory from which to load model checkpoints (walks directory and ensembles all models that are found)
-    checkpoint_path: str = None  # Path to model checkpoint (.pt file)
-    checkpoint_paths: List[str] = None  # List of paths to model checkpoints (.pt files)
-    no_cuda: bool = False  # Turn off cuda (i.e. use CPU instead of GPU)
-    gpu: int = None  # Which GPU to use
-    features_generator: List[str] = None  # Method(s) of generating additional features
-    features_path: List[str] = None  # Path(s) to features to use in FNN (instead of features_generator)
-    no_features_scaling: bool = False  # Turn off scaling of features
-    max_data_size: int = None  # Maximum number of data points to load
-    num_workers: int = 8   # Number of workers for the parallel data loading (0 means sequential)
-    batch_size: int = 50  # Batch size
+    smiles_column: str = None
+    """Name of the column containing SMILES strings. By default, uses the first column."""
+    checkpoint_dir: str = None
+    """Directory from which to load model checkpoints (walks directory and ensembles all models that are found)."""
+    checkpoint_path: str = None
+    """Path to model checkpoint (:code:`.pt` file)."""
+    checkpoint_paths: List[str] = None
+    """List of paths to model checkpoints (:code:`.pt` files)."""
+    no_cuda: bool = False
+    """Turn off cuda (i.e., use CPU instead of GPU)."""
+    gpu: int = None
+    """Which GPU to use."""
+    features_generator: List[str] = None
+    """Method(s) of generating additional features."""
+    features_path: List[str] = None
+    """Path(s) to features to use in FNN (instead of features_generator)."""
+    no_features_scaling: bool = False
+    """Turn off scaling of features."""
+    max_data_size: int = None
+    """Maximum number of data points to load."""
+    num_workers: int = 8 
+    """Number of workers for the parallel data loading (0 means sequential)."""
+    batch_size: int = 50
+    """Batch size."""
 
     @property
     def device(self) -> torch.device:
@@ -118,55 +130,116 @@ class TrainArgs(CommonArgs):
     """:class:`TrainArgs` includes :class:`CommonArgs` along with additional arguments used for training a Chemprop model."""
 
     # General arguments
-    data_path: str  # Path to data CSV file
-    target_columns: List[str] = None  # Name of the columns containing target values. By default, uses all columns except the SMILES column and the ignore columns.
-    ignore_columns: List[str] = None  # Name of the columns to ignore when target_columns is not provided.
-    dataset_type: Literal['regression', 'classification', 'multiclass']  # Type of dataset. This determines the loss function used during training.
-    multiclass_num_classes: int = 3  # Number of classes when running multiclass classification
-    separate_val_path: str = None  # Path to separate val set, optional
-    separate_test_path: str = None  # Path to separate test set, optional
-    split_type: Literal['random', 'scaffold_balanced', 'predetermined', 'crossval', 'index_predetermined'] = 'random'  # Method of splitting the data into train/val/test
-    split_sizes: Tuple[float, float, float] = (0.8, 0.1, 0.1)  # Split proportions for train/validation/test sets
-    num_folds: int = 1  # Number of folds when performing cross validation
-    folds_file: str = None  # Optional file of fold labels
-    val_fold_index: int = None  # Which fold to use as val for leave-one-out cross val
-    test_fold_index: int = None  # Which fold to use as test for leave-one-out cross val
-    crossval_index_dir: str = None  # Directory in which to find cross validation index files
-    crossval_index_file: str = None  # Indices of files to use as train/val/test. Overrides --num_folds and --seed.
-    seed: int = 0  # Random seed to use when splitting data into train/val/test sets. When `num_folds` > 1, the first fold uses this seed and all subsequent folds add 1 to the seed.
-    pytorch_seed: int = 0  # Seed for PyTorch randomness (e.g. random initial weights)
-    metric: Literal['auc', 'prc-auc', 'rmse', 'mae', 'mse', 'r2', 'accuracy', 'cross_entropy'] = None  # Metric to use during evaluation. Defaults to "auc" for classification and "rmse" for regression.
-    save_dir: str = None  # Directory where model checkpoints will be saved
-    save_smiles_splits: bool = False  # Save smiles for each train/val/test splits for prediction convenience later
-    test: bool = False  # Whether to skip training and only test the model
-    quiet: bool = False  # Skip non-essential print statements
-    log_frequency: int = 10  # The number of batches between each logging of the training loss
-    show_individual_scores: bool = False  # Show all scores for individual targets, not just average, at the end
-    cache_cutoff: int = 10000  # Maximum number of molecules in dataset to allow caching. Below this number, caching is used and data loading is sequential. Above this number, caching is not used and data loading is parallel.
+    data_path: str
+    """Path to data CSV file."""
+    target_columns: List[str] = None
+    """
+    Name of the columns containing target values.
+    By default, uses all columns except the SMILES column and the :code:`ignore_columns`.
+    """
+    ignore_columns: List[str] = None
+    """Name of the columns to ignore when :code:`target_columns` is not provided."""
+    dataset_type: Literal['regression', 'classification', 'multiclass']
+    """Type of dataset. This determines the loss function used during training."""
+    multiclass_num_classes: int = 3
+    """Number of classes when running multiclass classification."""
+    separate_val_path: str = None
+    """Path to separate val set, optional."""
+    separate_test_path: str = None
+    """Path to separate test set, optional."""
+    split_type: Literal['random', 'scaffold_balanced', 'predetermined', 'crossval', 'index_predetermined'] = 'random'
+    """Method of splitting the data into train/val/test."""
+    split_sizes: Tuple[float, float, float] = (0.8, 0.1, 0.1)
+    """Split proportions for train/validation/test sets."""
+    num_folds: int = 1
+    """Number of folds when performing cross validation."""
+    folds_file: str = None
+    """Optional file of fold labels."""
+    val_fold_index: int = None
+    """Which fold to use as val for leave-one-out cross val."""
+    test_fold_index: int = None
+    """Which fold to use as test for leave-one-out cross val."""
+    crossval_index_dir: str = None
+    """Directory in which to find cross validation index files."""
+    crossval_index_file: str = None
+    """Indices of files to use as train/val/test. Overrides :code:`--num_folds` and :code:`--seed`."""
+    seed: int = 0
+    """
+    Random seed to use when splitting data into train/val/test sets.
+    When :code`num_folds > 1`, the first fold uses this seed and all subsequent folds add 1 to the seed.
+    """
+    pytorch_seed: int = 0
+    """Seed for PyTorch randomness (e.g., random initial weights)."""
+    metric: Literal['auc', 'prc-auc', 'rmse', 'mae', 'mse', 'r2', 'accuracy', 'cross_entropy'] = None
+    """Metric to use during evaluation. Defaults to "auc" for classification and "rmse" for regression."""
+    save_dir: str = None
+    """Directory where model checkpoints will be saved."""
+    save_smiles_splits: bool = False
+    """Save smiles for each train/val/test splits for prediction convenience later."""
+    test: bool = False
+    """Whether to skip training and only test the model."""
+    quiet: bool = False
+    """Skip non-essential print statements."""
+    log_frequency: int = 10
+    """The number of batches between each logging of the training loss."""
+    show_individual_scores: bool = False
+    """Show all scores for individual targets, not just average, at the end."""
+    cache_cutoff: int = 10000
+    """
+    Maximum number of molecules in dataset to allow caching.
+    Below this number, caching is used and data loading is sequential.
+    Above this number, caching is not used and data loading is parallel.
+    """
 
     # Model arguments
-    bias: bool = False  # Whether to add bias to linear layers
-    hidden_size: int = 300  # Dimensionality of hidden layers in MPN
-    depth: int = 3  # Number of message passing steps
-    dropout: float = 0.0  # Dropout probability
-    activation: Literal['ReLU', 'LeakyReLU', 'PReLU', 'tanh', 'SELU', 'ELU'] = 'ReLU'  # Activation function
-    atom_messages: bool = False  # Centers messages on atoms instead of on bonds
-    undirected: bool = False  # Undirected edges (always sum the two relevant bond vectors)
-    ffn_hidden_size: int = None  # Hidden dim for higher-capacity FFN (defaults to hidden_size)
-    ffn_num_layers: int = 2  # Number of layers in FFN after MPN encoding
-    features_only: bool = False  # Use only the additional features in an FFN, no graph network
-    separate_val_features_path: List[str] = None  # Path to file with features for separate val set
-    separate_test_features_path: List[str] = None  # Path to file with features for separate test set
-    config_path: str = None  # Path to a .json file containing arguments. Any arguments present in the config file will override arguments specified via the command line or by the defaults.
-    ensemble_size: int = 1  # Number of models in ensemble
+    bias: bool = False
+    """Whether to add bias to linear layers."""
+    hidden_size: int = 300
+    """Dimensionality of hidden layers in MPN."""
+    depth: int = 3
+    """Number of message passing steps."""
+    dropout: float = 0.0
+    """Dropout probability."""
+    activation: Literal['ReLU', 'LeakyReLU', 'PReLU', 'tanh', 'SELU', 'ELU'] = 'ReLU'
+    """Activation function."""
+    atom_messages: bool = False
+    """Centers messages on atoms instead of on bonds."""
+    undirected: bool = False
+    """Undirected edges (always sum the two relevant bond vectors)."""
+    ffn_hidden_size: int = None
+    """Hidden dim for higher-capacity FFN (defaults to hidden_size)."""
+    ffn_num_layers: int = 2
+    """Number of layers in FFN after MPN encoding."""
+    features_only: bool = False
+    """Use only the additional features in an FFN, no graph network."""
+    separate_val_features_path: List[str] = None
+    """Path to file with features for separate val set."""
+    separate_test_features_path: List[str] = None
+    """Path to file with features for separate test set."""
+    config_path: str = None
+    """
+    Path to a :code:`.json` file containing arguments. Any arguments present in the config file
+    will override arguments specified via the command line or by the defaults.
+    """
+    ensemble_size: int = 1
+    """Number of models in ensemble."""
 
     # Training arguments
-    epochs: int = 30  # Number of epochs to run
-    warmup_epochs: float = 2.0  # Number of epochs during which learning rate increases linearly from init_lr to max_lr. Afterwards, learning rate decreases exponentially from max_lr to final_lr.
-    init_lr: float = 1e-4  # Initial learning rate
-    max_lr: float = 1e-3  # Maximum learning rate
-    final_lr: float = 1e-4  # Final learning rate
-    class_balance: bool = False  # Trains with an equal number of positives and negatives in each batch (only for single task classification)
+    epochs: int = 30
+    """Number of epochs to run."""
+    warmup_epochs: float = 2.0
+    """
+    Number of epochs during which learning rate increases linearly from :code:`init_lr` to :code:`max_lr`.
+    Afterwards, learning rate decreases exponentially from :code:`max_lr` to :code:`final_lr`.
+    """
+    init_lr: float = 1e-4
+    """Initial learning rate."""
+    max_lr: float = 1e-3
+    """Maximum learning rate."""
+    final_lr: float = 1e-4
+    """Final learning rate."""
+    class_balance: bool = False
+    """Trains with an equal number of positives and negatives in each batch (only for single task classification)."""
 
     def __init__(self, *args, **kwargs) -> None:
         super(TrainArgs, self).__init__(*args, **kwargs)
@@ -305,8 +378,10 @@ class TrainArgs(CommonArgs):
 class PredictArgs(CommonArgs):
     """:class:`PredictArgs` includes :class:`CommonArgs` along with additional arguments used for predicting with a Chemprop model."""
 
-    test_path: str  # Path to CSV file containing testing data for which predictions will be made
-    preds_path: str  # Path to CSV file where predictions will be saved
+    test_path: str
+    """Path to CSV file containing testing data for which predictions will be made."""
+    preds_path: str
+    """Path to CSV file where predictions will be saved."""
 
     @property
     def ensemble_size(self) -> int:
@@ -324,14 +399,22 @@ class PredictArgs(CommonArgs):
 class InterpretArgs(CommonArgs):
     """:class:`InterpretArgs` includes :class:`CommonArgs` along with additional arguments used for interpreting a trained Chemprop model."""
 
-    data_path: str  # Path to data CSV file
-    batch_size: int = 500  # Batch size
-    property_id: int = 1  # Index of the property of interest in the trained model
-    rollout: int = 20  # Number of rollout steps
-    c_puct: float = 10.0  # Constant factor in MCTS
-    max_atoms: int = 20  # Maximum number of atoms in rationale
-    min_atoms: int = 8  # Minimum number of atoms in rationale
-    prop_delta: float = 0.5  # Minimum score to count as positive
+    data_path: str
+    """Path to data CSV file."""
+    batch_size: int = 500
+    """Batch size."""
+    property_id: int = 1
+    """Index of the property of interest in the trained model."""
+    rollout: int = 20
+    """Number of rollout steps."""
+    c_puct: float = 10.0
+    """Constant factor in MCTS."""
+    max_atoms: int = 20
+    """Maximum number of atoms in rationale."""
+    min_atoms: int = 8
+    """Minimum number of atoms in rationale."""
+    prop_delta: float = 0.5
+    """Minimum score to count as positive."""
 
     def process_args(self) -> None:
         super(InterpretArgs, self).process_args()
@@ -349,31 +432,46 @@ class InterpretArgs(CommonArgs):
 class HyperoptArgs(TrainArgs):
     """:class:`HyperoptArgs` includes :class:`TrainArgs` along with additional arguments used for optimizing Chemprop hyperparameters."""
 
-    num_iters: int = 20  # Number of hyperparameter choices to try
-    config_save_path: str  # Path to .json file where best hyperparameter settings will be written
-    log_dir: str = None  # (Optional) Path to a directory where all results of the hyperparameter optimization will be written
+    num_iters: int = 20
+    """Number of hyperparameter choices to try."""
+    config_save_path: str
+    """Path to :code:`.json` file where best hyperparameter settings will be written."""
+    log_dir: str = None
+    """(Optional) Path to a directory where all results of the hyperparameter optimization will be written."""
 
 
 class SklearnTrainArgs(TrainArgs):
     """:class:`SklearnTrainArgs` includes :class:`TrainArgs` along with additional arguments for training a scikit-learn model."""
 
-    model_type: Literal['random_forest', 'svm']  # scikit-learn model to use
-    class_weight: Literal['balanced'] = None  # How to weight classes (None means no class balance)
-    single_task: bool = False  # Whether to run each task separately (needed when dataset has null entries)
-    radius: int = 2  # Morgan fingerprint radius
-    num_bits: int = 2048  # Number of bits in morgan fingerprint
-    num_trees: int = 500  # Number of random forest trees
+    model_type: Literal['random_forest', 'svm']
+    """scikit-learn model to use."""
+    class_weight: Literal['balanced'] = None
+    """How to weight classes (None means no class balance)."""
+    single_task: bool = False
+    """Whether to run each task separately (needed when dataset has null entries)."""
+    radius: int = 2
+    """Morgan fingerprint radius."""
+    num_bits: int = 2048
+    """Number of bits in morgan fingerprint."""
+    num_trees: int = 500
+    """Number of random forest trees."""
 
 
 class SklearnPredictArgs(Tap):
     """:class:`SklearnPredictArgs` contains arguments used for predicting with a trained scikit-learn model."""
 
-    test_path: str  # Path to CSV file containing testing data for which predictions will be made
-    smiles_column: str = None  # Name of the column containing SMILES strings. By default, uses the first column.
-    preds_path: str  # Path to CSV file where predictions will be saved
-    checkpoint_dir: str = None  # Path to directory containing model checkpoints (.pkl file)
-    checkpoint_path: str = None  # Path to model checkpoint (.pkl file)
-    checkpoint_paths: List[str] = None  # List of paths to model checkpoints (.pkl files)
+    test_path: str
+    """Path to CSV file containing testing data for which predictions will be made."""
+    smiles_column: str = None
+    """Name of the column containing SMILES strings. By default, uses the first column."""
+    preds_path: str
+    """Path to CSV file where predictions will be saved."""
+    checkpoint_dir: str = None
+    """Path to directory containing model checkpoints (:code:`.pkl` file)"""
+    checkpoint_path: str = None
+    """Path to model checkpoint (:code:`.pkl` file)"""
+    checkpoint_paths: List[str] = None
+    """List of paths to model checkpoints (:code:`.pkl` files)"""
 
     def process_args(self) -> None:
         # Load checkpoint paths
--- docs/source/args.rst
+++ docs/source/args.rst
@@ -3,7 +3,52 @@
 Command Line Arguments
 ======================
 
-`chemprop.train <https://github.com/chemprop/chemprop/tree/master/chemprop/train>`_ contains all command line arguments, which are processed using the `Typed Argument Parser <https://github.com/swansonk14/typed-argument-parser>`_ (:code:`Tap`) package.
+`chemprop.args.py <https://github.com/chemprop/chemprop/tree/master/chemprop/args.py>`_ contains all command line arguments, which are processed using the `Typed Argument Parser <https://github.com/swansonk14/typed-argument-parser>`_ (:code:`Tap`) package.
+
+Common Arguments
+----------------
+
+.. autoclass:: chemprop.args.CommonArgs
+   :members:
+
+Train Arguments
+---------------
+
+.. autoclass:: chemprop.args.TrainArgs
+   :members:
+
+Predict Arguments
+-----------------
+
+.. autoclass:: chemprop.args.PredictArgs
+   :members:
+
+Interpret Arguments
+-------------------
+
+.. autoclass:: chemprop.args.InterpretArgs
+   :members:
+
+Hyperparameter Optimization Arguments
+-------------------------------------
+
+.. autoclass:: chemprop.args.HyperoptArgs
+   :members:
+
+Scikit-Learn Train Arguments
+----------------------------
+
+.. autoclass:: chemprop.args.SklearnTrainArgs
+   :members:
+
+Scikit-Learn Predict Arguments
+------------------------------
+
+.. autoclass:: chemprop.args.SklearnPredictArgs
+   :members:
+
+Utility Functions
+-----------------
 
 .. automodule:: chemprop.args
-   :members:
+   :members: get_checkpoint_paths
