37ceacbec14322ebc37107cb302d5b00517bbced
Kyle Swanson <swansonk.14@gmail.com>
2020-07-24 20:06:16 -0400

    Fixing test file name for pytest

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ],
