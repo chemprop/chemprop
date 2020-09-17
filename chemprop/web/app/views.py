0eb642806da21f5c80d9beb1ccdeb8f93b463bfc
Kyle Swanson <swansonk.14@gmail.com>
2020-09-17 16:30:15 +0100

    Just after train

--- chemprop/web/app/views.py
+++ chemprop/web/app/views.py
@@ -255,8 +255,8 @@ def train():
 
         # Run training
         logger = create_logger(name=TRAIN_LOGGER_NAME, save_dir=args.save_dir, quiet=args.quiet)
-        return render_train()
         task_scores = run_training(args, data, logger)[args.metrics[0]]
+        return render_train()
         process.join()
 
         # Reset globals
