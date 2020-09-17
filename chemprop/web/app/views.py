d58f6194f997d0f603017c8acec324194110f46a
Kyle Swanson <swansonk.14@gmail.com>
2020-09-17 16:12:01 +0100

    All except train

--- chemprop/web/app/views.py
+++ chemprop/web/app/views.py
@@ -255,10 +255,9 @@ def train():
 
         # Run training
         logger = create_logger(name=TRAIN_LOGGER_NAME, save_dir=args.save_dir, quiet=args.quiet)
+        return render_train()
         task_scores = run_training(args, data, logger)[args.metrics[0]]
         process.join()
-
-        return render_train()
 
         # Reset globals
         TRAINING = 0
