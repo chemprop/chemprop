de4e49241d64b7ac80256ea14406ee72564c4bad
Kyle Swanson <swansonk.14@gmail.com>
2020-09-17 18:30:30 +0100

    Stop right before logger

--- chemprop/web/app/views.py
+++ chemprop/web/app/views.py
@@ -256,8 +256,8 @@ def train():
             TRAINING = 1
 
         # Run training
-        logger = create_logger(name=TRAIN_LOGGER_NAME, save_dir=args.save_dir, quiet=args.quiet)
         return render_train()
+        logger = create_logger(name=TRAIN_LOGGER_NAME, save_dir=args.save_dir, quiet=args.quiet)
         task_scores = run_training(args, data, logger)[args.metrics[0]]
 
         if use_progress_bar:
