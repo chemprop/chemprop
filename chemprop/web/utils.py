fc35354a629c5294d2c14e800b54feedce86272b
Kyle Swanson <swansonk.14@gmail.com>
2020-09-22 18:22:25 +0100

    Adding better error message regarding web root folder access, thanks to msteijaert

--- chemprop/web/utils.py
+++ chemprop/web/utils.py
@@ -24,6 +24,10 @@ def set_root_folder(app: Flask, root_folder: str = None, create_folders: bool = 
 
     # Create folders
     if create_folders:
+        if not os.access(os.path.dirname(app.config['ROOT_FOLDER']), os.W_OK):
+            raise ValueError(f'You do not have write permissions on the root_folder: {app.config["ROOT_FOLDER"]}\n'
+                             f'Please specify a different root_folder while starting the web app.')
+
         for folder_name in ['ROOT_FOLDER', 'DATA_FOLDER', 'CHECKPOINT_FOLDER', 'TEMP_FOLDER']:
             os.makedirs(app.config[folder_name], exist_ok=True)
 
