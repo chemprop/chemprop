5780f558d1f528b83bbb95634803bc3ff0292e7d
Florence Vermeire <fhvermei@mit.edu>
2021-01-28 20:22:57 -0500

    Add extra bond features to predict.py

--- chemprop/train/predict.py
+++ chemprop/train/predict.py
@@ -27,11 +27,12 @@ def predict(model: MoleculeModel,
     for batch in tqdm(data_loader, disable=disable_progress_bar, leave=False):
         # Prepare batch
         batch: MoleculeDataset
-        mol_batch, features_batch, atom_descriptors_batch = batch.batch_graph(), batch.features(), batch.atom_descriptors()
+        mol_batch, features_batch, atom_descriptors_batch, bond_descriptors_batch = \
+            batch.batch_graph(), batch.features(), batch.atom_descriptors(), batch.bond_descriptors()
 
         # Make predictions
         with torch.no_grad():
-            batch_preds = model(mol_batch, features_batch, atom_descriptors_batch)
+            batch_preds = model(mol_batch, features_batch, atom_descriptors_batch, bond_descriptors_batch)
 
         batch_preds = batch_preds.data.cpu().numpy()
 
