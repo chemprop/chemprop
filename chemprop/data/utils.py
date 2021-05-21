9948583236b708d59c595a15c24a5591de9fcab8
Chas <charlesjmcgill@gmail.com>
2021-05-21 15:30:41 -0400

    Add training weights for different targets and different datapoints
    Add data weights to save_smiles_splits if used
    
    
    Add multiclass data weighting
    
    
    Update Readme for Weights and Frozen Layers

--- chemprop/data/utils.py
+++ chemprop/data/utils.py
@@ -98,6 +98,22 @@ def get_header(path: str) -> List[str]:
     return header
 
 
+def get_data_weights(path: str) -> List[float]:
+    """
+    Returns the list of data weights for the loss function as stored in a CSV file.
+
+    :param path: Path to a CSV file.
+    :return: A list of floats containing the data weights.
+    """
+    weights = []
+    with open(path) as f:
+        reader=csv.reader(f)
+        next(reader) #skip header row
+        for line in reader:
+            weights.append(float(line[0]))
+    return weights
+
+
 def get_smiles(path: str,
                smiles_columns: Union[str, List[str]] = None,
                header: bool = True,
@@ -153,6 +169,7 @@ def get_data(path: str,
              ignore_columns: List[str] = None,
              skip_invalid_smiles: bool = True,
              args: Union[TrainArgs, PredictArgs] = None,
+             data_weights_path: str = None,
              features_path: List[str] = None,
              features_generator: List[str] = None,
              atom_descriptors_path: str = None,
@@ -172,6 +189,7 @@ def get_data(path: str,
     :param ignore_columns: Name of the columns to ignore when :code:`target_columns` is not provided.
     :param skip_invalid_smiles: Whether to skip and filter out invalid smiles using :func:`filter_invalid_smiles`.
     :param args: Arguments, either :class:`~chemprop.args.TrainArgs` or :class:`~chemprop.args.PredictArgs`.
+    :param data_weights_path: A path to a file containing weights for each molecule in the loss function.
     :param features_path: A list of paths to files containing features. If provided, it is used
                           in place of :code:`args.features_path`.
     :param features_generator: A list of features generators to use. If provided, it is used
@@ -193,6 +211,7 @@ def get_data(path: str,
         smiles_columns = smiles_columns if smiles_columns is not None else args.smiles_columns
         target_columns = target_columns if target_columns is not None else args.target_columns
         ignore_columns = ignore_columns if ignore_columns is not None else args.ignore_columns
+        data_weights_path = data_weights_path if data_weights_path is not None else args.data_weights_path
         features_path = features_path if features_path is not None else args.features_path
         features_generator = features_generator if features_generator is not None else args.features_generator
         atom_descriptors_path = atom_descriptors_path if atom_descriptors_path is not None \
@@ -215,6 +234,12 @@ def get_data(path: str,
     else:
         features_data = None
 
+    # Load data weights
+    if data_weights_path is not None:
+        data_weights = get_data_weights(data_weights_path)
+    else:
+        data_weights = None
+
     # Load data
     with open(path) as f:
         reader = csv.DictReader(f)
@@ -228,7 +253,7 @@ def get_data(path: str,
                 ignore_columns=ignore_columns,
             )
 
-        all_smiles, all_targets, all_rows, all_features = [], [], [], []
+        all_smiles, all_targets, all_rows, all_features, all_weights = [], [], [], [], []
         for i, row in enumerate(tqdm(reader)):
             smiles = [row[c] for c in smiles_columns]
 
@@ -243,6 +268,9 @@ def get_data(path: str,
 
             if features_data is not None:
                 all_features.append(features_data[i])
+
+            if data_weights is not None:
+                all_weights.append(data_weights[i])
 
             if store_row:
                 all_rows.append(row)
@@ -275,6 +303,7 @@ def get_data(path: str,
                 smiles=smiles,
                 targets=targets,
                 row=all_rows[i] if store_row else None,
+                data_weight=all_weights[i] if data_weights is not None else 1.,
                 features_generator=features_generator,
                 features=all_features[i] if features_data is not None else None,
                 atom_features=atom_features[i] if atom_features is not None else None,
