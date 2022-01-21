033058c40c963128046b74c194512ad8d0ebaffe
eyalmazuz <mazuze@post.bgu.ac.il>
2022-01-21 08:23:17 +0200

    fixed parallel issue with ind_preds variable

--- chemprop/train/make_predictions.py
+++ chemprop/train/make_predictions.py
@@ -208,8 +208,8 @@ def predict_and_save(args: PredictArgs, train_args: TrainArgs, test_data: Molecu
         if args.dataset_type == 'multiclass':
             if isinstance(preds, np.ndarray) and preds.ndim > 1:
                 preds = preds.reshape((num_tasks))
-            if args.ensemble_variance or args. individual_ensemble_predictions:
-                ind_preds = ind_preds.reshape((num_tasks, len(args.checkpoint_paths)))
+                if args.ensemble_variance or args. individual_ensemble_predictions:
+                    ind_preds = ind_preds.reshape((num_tasks, len(args.checkpoint_paths)))
 
         # If extra columns have been dropped, add back in SMILES columns
         if args.drop_extra_columns:
