d2eb3faa1eb03732b39253b68875fd78d7df2493
Max Liu <mjliu@mit.edu>
2022-05-12 17:27:33 -0400

    Bump minimum Python version to 3.7

--- setup.py
+++ setup.py
@@ -57,10 +57,9 @@ setup(
             'parameterized>=0.8.1'
         ]
     },
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
