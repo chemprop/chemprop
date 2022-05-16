56adc3b8896cebce40dde5604d74c8405a5ad1e7
david graff <deg711@g.harvard.edu>
2022-05-16 10:38:37 -0400

    improve reaction encoder style

--- chemprop/models/v2/encoders/reaction.py
+++ chemprop/models/v2/encoders/reaction.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from copy import deepcopy
+import warnings
 from typing import Iterable
 
 import torch
@@ -8,22 +8,34 @@ from torch import Tensor, nn
 
 from chemprop.models.v2.encoders.base import MPNEncoder, MoleculeEncoder
 from chemprop.models.v2.encoders.molecule import build_molecule_encoder
-from chemprop.nn_utils import weight_init
 
 
 class ReactionEncoder(MPNEncoder):
-    def __init__(
-        self, encoder: MoleculeEncoder, n_mols: int, shared: bool = False, reinitialize: bool = True
-    ):
+    def __init__(self, encoders: Iterable[MoleculeEncoder], n_mols: int, shared: bool = False):
         super().__init__()
 
-        self.n_mols = n_mols
+        if len(encoders) == 0:
+            raise ValueError("arg `encoders` was empty!")
+
         if shared:
-            self.encoders = nn.ModuleList([encoder for _ in range(n_mols)])
+            if len(encoders) > 1:
+                warnings.warn(
+                    "More than 1 encoder was supplied but `shared` was True! "
+                    "Using only the 0th encoder."
+                )
+        elif len(encoders) != n_mols:
+            raise ValueError(
+                "arg `n_mols` must be equal to `len(encoders)` if `shared` is False! "
+                f"got: {n_mols} and {len(encoders)}, respectively."
+            )
+
+        self.n_mols = n_mols
+        self.shared = shared
+
+        if self.shared:
+            self.encoders = nn.ModuleList([encoders[0]] * n_mols)
         else:
-            self.encoders = nn.ModuleList([deepcopy(encoder) for _ in range(n_mols)])
-            # if reinitialize:
-            #     [encoder.apply(weight_init) for encoder in self.encoders]
+            self.encoders = nn.ModuleList(encoders)
 
         self.__output_dim = sum(encoder.output_dim for encoder in self.encoders)
 
@@ -56,8 +68,8 @@ class ReactionEncoder(MPNEncoder):
         Returns
         -------
         Tensor
-            a Tensor of shape `b x d_h` containing the reaction encodings, where `b` is the number
-            of reactions in the batch, and `d_h` is equal to `output_dim`
+            a Tensor of shape `b x d_o` containing the reaction encodings, where `b` is the number
+            of reactions in the batch, and `d_o` is the `output_dim` of the encoder
             (== `self.n_mols x self.encoders[0].output_dim`)
         """
         Hs = [encoder(*inputs) for encoder, inputs in zip(self.encoders, reactant_batches)]
@@ -67,8 +79,11 @@ class ReactionEncoder(MPNEncoder):
 
     @classmethod
     def from_mol_encoder_args(
-        cls, n_mols: int, shared: bool = False, reinitialize: bool = True, *args, **kwargs
+        cls, n_mols: int, shared: bool = False, *args, **kwargs
     ) -> ReactionEncoder:
-        encoder = build_molecule_encoder(*args, **kwargs)
+        if not shared:
+            encoders = [build_molecule_encoder(*args, **kwargs) for _ in range(n_mols)]
+        else:
+            encoders = [build_molecule_encoder(*args, **kwargs)]
 
-        return cls(encoder, n_mols, shared, reinitialize)
+        return cls(encoders, n_mols, shared)
