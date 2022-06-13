9a44c2d946b69a8c23a81ccc8ba69576891b1358
Charles McGill <44245643+cjmcgill@users.noreply.github.com>
2022-06-13 16:14:36 -0400

    Multitask Evaluation on Different Magnitude Targets (#290)
    * Create multitask mean utils function
    
    * Use multitask_mean in place of np mean and remove nanmeans
    
    * Change testing dtypes to floats
    
    * Remove nan filters from testing
    
    * Add informative error for new metrics
    
    * Message in log file about nan results

--- chemprop/train/cross_validate.py
+++ chemprop/train/cross_validate.py
@@ -6,6 +6,7 @@ import os
 import sys
 from typing import Callable, Dict, List, Tuple
 import subprocess
+
 import numpy as np
 import pandas as pd
 
@@ -60,7 +61,7 @@ def cross_validate(args: TrainArgs,
         debug('Could not write the reproducibility section of the arguments to file, thus omitting this section.')
         args.save(os.path.join(args.save_dir, 'args.json'), with_reproducibility=False)
 
-    #set explicit H option and reaction option
+    # set explicit H option and reaction option
     reset_featurization_parameters(logger=logger)
     set_explicit_h(args.explicit_h)
     set_adding_hs(args.adding_h)
@@ -68,7 +69,7 @@ def cross_validate(args: TrainArgs,
         set_reaction(args.reaction, args.reaction_mode)
     elif args.reaction_solvent:
         set_reaction(True, args.reaction_mode)
-        
+    
     # Get data
     debug('Loading data')
     data = get_data(
@@ -127,24 +128,36 @@ def cross_validate(args: TrainArgs,
     info(f'{args.num_folds}-fold cross validation')
 
     # Report scores for each fold
+    contains_nan_scores = False
     for fold_num in range(args.num_folds):
         for metric, scores in all_scores.items():
-            info(f'\tSeed {init_seed + fold_num} ==> test {metric} = {np.nanmean(scores[fold_num]):.6f}')
+            info(f'\tSeed {init_seed + fold_num} ==> test {metric} = {multitask_mean(scores[fold_num], metric):.6f}')
 
             if args.show_individual_scores:
                 for task_name, score in zip(args.task_names, scores[fold_num]):
                     info(f'\t\tSeed {init_seed + fold_num} ==> test {task_name} {metric} = {score:.6f}')
+                    if np.isnan(score):
+                        contains_nan_scores = True
 
     # Report scores across folds
     for metric, scores in all_scores.items():
-        avg_scores = np.nanmean(scores, axis=1)  # average score for each model across tasks
-        mean_score, std_score = np.nanmean(avg_scores), np.nanstd(avg_scores)
+        avg_scores = multitask_mean(scores, axis=1, metric=metric)  # average score for each model across tasks
+        mean_score, std_score = np.mean(avg_scores), np.std(avg_scores)
         info(f'Overall test {metric} = {mean_score:.6f} +/- {std_score:.6f}')
 
         if args.show_individual_scores:
             for task_num, task_name in enumerate(args.task_names):
                 info(f'\tOverall test {task_name} {metric} = '
-                     f'{np.nanmean(scores[:, task_num]):.6f} +/- {np.nanstd(scores[:, task_num]):.6f}')
+                     f'{np.mean(scores[:, task_num]):.6f} +/- {np.std(scores[:, task_num]):.6f}')
+
+    if contains_nan_scores:
+        info("The metric scores observed for some fold test splits contain 'nan' values. \
+            This can occur when the test set does not meet the requirements \
+            for a particular metric, such as having no valid instances of one \
+            task in the test set or not having positive examples for some classification metrics. \
+            Before v1.5.1, the default behavior was to ignore nan values in individual folds or tasks \
+            and still return an overall average for the remaining folds or tasks. The behavior now \
+            is to include them in the average, converting overall average metrics to 'nan' as well.")
 
     # Save scores
     with open(os.path.join(save_dir, TEST_SCORES_FILE_NAME), 'w') as f:
@@ -160,7 +173,7 @@ def cross_validate(args: TrainArgs,
             row = ['spectra']
             for metric, scores in all_scores.items():
                 task_scores = scores[:,0]
-                mean, std = np.nanmean(task_scores), np.nanstd(task_scores)
+                mean, std = np.mean(task_scores), np.std(task_scores)
                 row += [mean, std] + task_scores.tolist()
             writer.writerow(row)
         else: # all other data types, separate scores by task
@@ -168,13 +181,13 @@ def cross_validate(args: TrainArgs,
                 row = [task_name]
                 for metric, scores in all_scores.items():
                     task_scores = scores[:, task_num]
-                    mean, std = np.nanmean(task_scores), np.nanstd(task_scores)
+                    mean, std = np.mean(task_scores), np.std(task_scores)
                     row += [mean, std] + task_scores.tolist()
                 writer.writerow(row)
 
     # Determine mean and std score of main metric
-    avg_scores = np.nanmean(all_scores[args.metric], axis=1)
-    mean_score, std_score = np.nanmean(avg_scores), np.nanstd(avg_scores)
+    avg_scores = multitask_mean(all_scores[args.metric], metric=args.metric, axis=1)
+    mean_score, std_score = np.mean(avg_scores), np.std(avg_scores)
 
     # Optionally merge and save test preds
     if args.save_preds:
--- chemprop/train/run_training.py
+++ chemprop/train/run_training.py
@@ -297,10 +297,10 @@ def run_training(args: TrainArgs,
             )
 
             for metric, scores in val_scores.items():
-                # Average validation score
-                avg_val_score = np.nanmean(scores)
-                debug(f'Validation {metric} = {avg_val_score:.6f}')
-                writer.add_scalar(f'validation_{metric}', avg_val_score, n_iter)
+                # Average validation score\
+                mean_val_score = multitask_mean(scores, metric=metric)
+                debug(f'Validation {metric} = {mean_val_score:.6f}')
+                writer.add_scalar(f'validation_{metric}', mean_val_score, n_iter)
 
                 if args.show_individual_scores:
                     # Individual validation scores
@@ -309,10 +309,10 @@ def run_training(args: TrainArgs,
                         writer.add_scalar(f'validation_{task_name}_{metric}', val_score, n_iter)
 
             # Save model checkpoint if improved validation score
-            avg_val_score = np.nanmean(val_scores[args.metric])
-            if args.minimize_score and avg_val_score < best_score or \
-                    not args.minimize_score and avg_val_score > best_score:
-                best_score, best_epoch = avg_val_score, epoch
+            mean_val_score = multitask_mean(val_scores[args.metric], metric=args.metric)
+            if args.minimize_score and mean_val_score < best_score or \
+                    not args.minimize_score and mean_val_score > best_score:
+                best_score, best_epoch = mean_val_score, epoch
                 save_checkpoint(os.path.join(save_dir, MODEL_FILE_NAME), model, scaler, features_scaler,
                                 atom_descriptor_scaler, bond_feature_scaler, args)
 
@@ -376,8 +376,8 @@ def run_training(args: TrainArgs,
 
     for metric, scores in ensemble_scores.items():
         # Average ensemble score
-        avg_ensemble_test_score = np.nanmean(scores)
-        info(f'Ensemble test {metric} = {avg_ensemble_test_score:.6f}')
+        mean_ensemble_test_score = multitask_mean(scores, metric=metric)
+        info(f'Ensemble test {metric} = {mean_ensemble_test_score:.6f}')
 
         # Individual ensemble scores
         if args.show_individual_scores:
