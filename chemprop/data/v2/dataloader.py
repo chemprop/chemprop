32ebc54d303ddf2c729bb75679a0ecc41403063f
david graff <deg711@g.harvard.edu>
2022-10-09 19:01:16 -0400

    refactor data loading and encoder input to utilize `BatchMolGraph` object

--- chemprop/data/v2/dataloader.py
+++ chemprop/data/v2/dataloader.py
@@ -1,67 +1,32 @@
 from __future__ import annotations
+import pdb
 
 from typing import Iterable, Optional
 
 import numpy as np
 import torch
+from torch import Tensor
 from torch.utils.data import DataLoader
 
 from chemprop.data.v2.datasets import MolGraphDatasetBase
 from chemprop.data.v2.samplers import ClassBalanceSampler, SeededSampler
 from chemprop.featurizers.v2 import MolGraph
-from chemprop.models.v2 import MoleculeEncoderInput
+from chemprop.featurizers.v2.molgraph import BatchMolGraph
+
+TrainingBatch = tuple[BatchMolGraph, Tensor, Tensor, Optional[Tensor], Optional[Tensor]]
 
 
 def collate_batch(
     batch: Iterable[tuple[MolGraph, np.ndarray, float, np.ndarray, np.ndarray]]
-) -> tuple[MoleculeEncoderInput, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+) -> TrainingBatch:
     mgs, ys, weights, gt_targets, lt_targets = zip(*batch)
 
-    n_atoms = 1
-    n_bonds = 1
-    a_scope = []
-    b_scope = []
-
-    # All start with zero padding so that indexing with zero padding returns zeros
-    X_vs = [np.zeros((1, mgs[0].X_v.shape[1]))]
-    X_es = [np.zeros((1, mgs[0].X_e.shape[1]))]
-    a2b = [[]]
-    b2a = [0]
-    b2revb = [0]
-
-    for mg in mgs:
-        X_vs.append(mg.X_v)
-        X_es.append(mg.X_e)
-
-        a2b.extend([b + n_bonds for b in mg.a2b[a]] for a in range(mg.n_atoms))
-        b2a.extend(n_atoms + mg.b2a[b] for b in range(mg.n_bonds))
-        b2revb.extend(n_bonds + mg.b2revb[b] for b in range(mg.n_bonds))
-
-        a_scope.append((n_atoms, mg.n_atoms))
-        b_scope.append((n_bonds, mg.n_bonds))
-
-        n_atoms += mg.n_atoms
-        n_bonds += mg.n_bonds
-
-    X_v = torch.from_numpy(np.concatenate(X_vs)).float()
-    X_e = torch.from_numpy(np.concatenate(X_es)).float()
-
-    # max with 1 to fix a crash in rare case of all single-heavy-atom mols
-    max_num_bonds = max(1, max(len(in_bonds) for in_bonds in a2b))
-    a2b = torch.tensor(
-        [a2b[a] + [0] * (max_num_bonds - len(a2b[a])) for a in range(n_atoms)], dtype=torch.long
-    )
-
-    b2a = torch.tensor(b2a, dtype=torch.long)
-    b2revb = torch.tensor(b2revb, dtype=torch.long)
-    a2a = b2a[a2b]
-
     return (
-        (X_v, X_e, a2b, b2a, b2revb, a_scope, b_scope, a2a),
-        np.array(ys, dtype=float),
-        np.array(weights),
-        None if lt_targets[0] is None else np.array(lt_targets),
-        None if gt_targets[0] is None else np.array(gt_targets)
+        BatchMolGraph(mgs),
+        torch.from_numpy(np.array(ys, "f4")),
+        torch.from_numpy(np.array(weights, "f4")),
+        None if lt_targets[0] is None else torch.from_numpy(np.array(lt_targets)),
+        None if gt_targets[0] is None else torch.from_numpy(np.array(gt_targets))
     )
 
 
@@ -70,7 +35,7 @@ class MolGraphDataLoader(DataLoader):
 
     Parameters
     ----------
-    dataset : MoleculeDataset
+    dset : MoleculeDataset
         The `MoleculeDataset` containing the molecules to load.
     batch_size : int, default=50
         the batch size to load
@@ -100,9 +65,9 @@ class MolGraphDataLoader(DataLoader):
         self.shuffle = shuffle
 
         if self.class_balance:
-            self.sampler = ClassBalanceSampler(self.dset, seed, self.shuffle)
+            self.sampler = ClassBalanceSampler(self.dset.targets, seed, self.shuffle)
         elif self.shuffle and seed is not None:
-            self.sampler = SeededSampler(self.dataset, seed)
+            self.sampler = SeededSampler(len(self.dset), seed)
         else:
             self.sampler = None
 
--- chemprop/models/v2/encoders/molecule.py
+++ chemprop/models/v2/encoders/molecule.py
@@ -1,26 +1,19 @@
 from __future__ import annotations
 
 from abc import abstractmethod
+from ctypes import Union
 from typing import Iterable, Optional
 
 import torch
 from torch import Tensor, nn
+from chemprop.featurizers.v2.molgraph import BatchMolGraph
 
 from chemprop.models.v2.encoders.base import MPNEncoder
+from chemprop.models.v2.encoders.utils import Aggregation
 from chemprop.nn_utils import get_activation_function
 
 
-MoleculeEncoderInput = tuple[
-    Tensor,
-    Tensor,
-    Tensor,
-    Tensor,
-    Tensor,
-    Iterable[tuple[int]],
-    Iterable[tuple[int]],
-    Optional[Tensor],
-    Optional[Tensor],
-]
+MoleculeEncoderInput = tuple[BatchMolGraph, Optional[Tensor]]
 
 
 class MoleculeEncoder(MPNEncoder):
@@ -35,7 +28,7 @@ class MoleculeEncoder(MPNEncoder):
         # layers_per_message: int,
         dropout: float = 0,
         activation: str = "relu",
-        aggregation: str = "mean",
+        aggregation: Union[str, Aggregation] = Aggregation.MEAN,
         atom_descriptors: Optional[str] = None,
         d_vd: Optional[int] = None,
     ):
@@ -47,7 +40,7 @@ class MoleculeEncoder(MPNEncoder):
 
         self.dropout = nn.Dropout(dropout)
         self.act = get_activation_function(activation)
-        self.aggregation = aggregation
+        self.agg = Aggregation.get(aggregation) if isinstance(aggregation, str) else aggregation
 
         self.cached_zero_vector = nn.Parameter(torch.zeros(d_h), requires_grad=False)
 
@@ -57,6 +50,24 @@ class MoleculeEncoder(MPNEncoder):
             self.d_vd = d_vd
             self.__output_dim += d_vd
             self.fc_vd = nn.Linear(d_h + d_vd, d_h + d_vd)
+
+        self.setup_weight_matrices(d_v, d_e, d_h, bias)
+
+    @abstractmethod
+    def setup_weight_matrices(self, d_v: int, d_e: int, d_h: int = 300, bias: bool = False):
+        """set up the weight matrices used in the message passing udpate functions
+        
+        Parameters
+        ----------
+        d_v : int
+            the input vertex feature dimension
+        d_e : int
+            the input edge feature dimension
+        d_h : int
+            the hidden dimension during message passing
+        bias: bool
+            whether to add a learned bias to the matrices
+        """
 
     @property
     def output_dim(self) -> int:
@@ -99,48 +110,25 @@ class MoleculeEncoder(MPNEncoder):
         h_vs = torch.split(H_v, sizes)
         hs = [h_v.sum(0) if size > 0 else self.cached_zero_vector for h_v, size in zip(h_vs, sizes)]
 
-        if self.aggregation == "mean":
+        if self.agg == Aggregation.MEAN:
             hs = [h / size if size > 0 else h for h, size in zip(hs, sizes)]
-        elif self.aggregation == "norm":
+        elif self.agg == Aggregation.NORM:
             hs = [h / self.aggregation_norm if size > 0 else h for h, size in zip(hs, sizes)]
+        elif self.agg == Aggregation.SUM:
+            pass
         else:
             raise RuntimeError
 
         return torch.stack(hs, 0)
 
     @abstractmethod
-    def forward(
-        self,
-        X_v: Tensor,
-        X_e: Tensor,
-        a2b: Tensor,
-        b2a: Tensor,
-        b2revb: Tensor,
-        a_scope: Iterable[tuple[int]],
-        b_scope: Iterable[tuple[int]],
-        a2a: Optional[Tensor] = None,
-        X_vd: Optional[Tensor] = None,
-    ) -> Tensor:
+    def forward(self, bmg: BatchMolGraph, X_vd: Optional[Tensor] = None) -> Tensor:
         """Encode a batch of molecular graphs.
 
         Parameters
         ----------
-        X_v : Tensor
-            a tensor of shape `V x d_v` containg atom ("vertex") features, where `V` is the number
-            of atoms in the batch + 1 and `d_v` is the dimension of the atom features
-        X_e : Tensor
-            a tensor of shape `E x d_e` containg bond ("edge") features, where `E` is two times the
-            number of directed edges in the batch and `d_e` is the dimension of the edge features
-        a2b : Tensor
-            a mapping from atom index to incoming bond indices
-        b2a : Tensor
-            a mapping from bond index to the index of the atom the bond is coming from
-        b2revb : Tensor
-            mapping from bond index to the index of the reverse bond
-        a_scope : Iterable[tuple[int]]
-            a list of tuples containing (start_index, num_atoms) for each molecule in the batch
-        b_scope : Iterable[tuple[int]]
-            TODO
+        bmg: BatchMolGraph
+            the batch of `MolGraphs` to encode
         X_vd : Optional[Tensor]
             an optional tensor of shape `V x d_vd` containing additional descriptors for each atom
             in the batch. These will be concatenated to the learned atomic descriptors and
@@ -156,144 +144,69 @@ class MoleculeEncoder(MPNEncoder):
 
 
 class BondMessageEncoder(MoleculeEncoder):
-    def __init__(
-        self,
-        d_v: int,
-        d_e: int,
-        d_h: int = 300,
-        bias: bool = False,
-        depth: int = 3,
-        undirected: bool = False,
-        dropout: float = 0,
-        activation: str = "relu",
-        aggregation: str = "mean",
-        atom_descriptors: Optional[str] = None,
-        d_vd: Optional[int] = None,
-    ):
-        super().__init__(
-            d_v,
-            d_e,
-            d_h,
-            bias,
-            depth,
-            undirected,
-            dropout,
-            activation,
-            aggregation,
-            atom_descriptors,
-            d_vd,
-        )
-
+    def setup_weight_matrices(self, d_v: int, d_e: int, d_h: int = 300, bias: bool = False):
         self.W_i = nn.Linear(d_e, d_h, bias)
         self.W_h = nn.Linear(d_h, d_h, bias)
         self.W_o = nn.Linear(d_v + d_h, d_h)
 
-    def forward(
-        self,
-        X_v: Tensor,
-        X_e: Tensor,
-        a2b: Tensor,
-        b2a: Tensor,
-        b2revb: Tensor,
-        a_scope: Iterable[tuple],
-        b_scope: Optional[Iterable[tuple]] = None,
-        a2a: Optional[Tensor] = None,
-        X_vd: Optional[Tensor] = None,
-    ) -> Tensor:
-        H_0 = self.W_i(X_e)  # E x d_h
+    def forward(self, bmg: BatchMolGraph, X_vd: Optional[Tensor] = None) -> Tensor:
+        H_0 = self.W_i(bmg.X_e)  # E x d_h
 
         H_e = self.act(H_0)
         for _ in range(1, self.depth):
             if self.undirected:
-                H_e = (H_e + H_e[b2revb]) / 2
+                H_e = (H_e + H_e[bmg.b2revb]) / 2
 
-            M_e = H_e[a2b].sum(1)[b2a] - H_e[b2revb]  # E x d_h
+            M_e = H_e[bmg.a2b].sum(1)[bmg.b2a] - H_e[bmg.b2revb]  # E x d_h
 
             H_e = H_0 + self.W_h(M_e)  # E x d_h
             H_e = self.act(H_e)
             H_e = self.dropout(H_e)
 
-        M_v = H_e[a2b].sum(1)  # V x d_h
-        H_v = self.W_o(torch.cat((X_v, M_v), 1))  # V x d_h
+        M_v = H_e[bmg.a2b].sum(1)  # V x d_h
+        H_v = self.W_o(torch.cat((bmg.X_v, M_v), 1))  # V x d_h
         H_v = self.act(H_v)
         H_v = self.dropout(H_v)
 
         if X_vd is not None:
             H_v = self.cat_descriptors(H_v, X_vd)
 
-        H = self.readout(H_v[1:], [n_a for _, n_a in a_scope])  # B x d_h + (d_vd)
+        H = self.readout(H_v[1:], [n_a for _, n_a in bmg.a_scope])  # B x d_h + (d_vd)
 
         return H
 
 
 class AtomMessageEncoder(MoleculeEncoder):
-    def __init__(
-        self,
-        d_v: int,
-        d_e: int,
-        d_h: int = 300,
-        bias: bool = False,
-        depth: int = 3,
-        undirected: bool = False,
-        dropout: float = 0,
-        activation: str = "relu",
-        aggregation: str = "mean",
-        atom_descriptors: Optional[str] = None,
-        d_vd: Optional[int] = None,
-    ):
-        super().__init__(
-            d_v,
-            d_e,
-            d_h,
-            bias,
-            depth,
-            undirected,
-            dropout,
-            activation,
-            aggregation,
-            atom_descriptors,
-            d_vd,
-        )
+    def setup_weight_matrices(self, d_v: int, d_e: int, d_h: int = 300, bias: bool = False):
         self.W_i = nn.Linear(d_v, d_h, bias)
         self.W_h = nn.Linear(d_e + d_h, d_h, bias)
         self.W_o = nn.Linear(d_v + d_h, d_h)
 
-    def forward(
-        self,
-        X_v: Tensor,
-        X_e: Tensor,
-        a2b: Tensor,
-        b2a: Tensor,
-        b2revb: Tensor,
-        a_scope: Iterable[tuple],
-        b_scope: Optional[Iterable[tuple]],
-        a2a: Tensor = None,
-        X_vd: Optional[Tensor] = None,
-    ) -> Tensor:
-        H_0 = self.W_i(X_v)  # V x d_h
+    def forward(self, bmg: BatchMolGraph, X_vd: Optional[Tensor] = None) -> Tensor:
+        H_0 = self.W_i(bmg.X_v)  # V x d_h
         H_v = self.act(H_0)
 
         for _ in range(1, self.depth):
             if self.undirected:
-                H_v = (H_v + H_v[b2revb]) / 2
+                H_v = (H_v + H_v[bmg.b2revb]) / 2
 
-            M_v_k = torch.cat((H_v[a2a], X_e[a2b]), 2)  # V x b x (d_h + d_e)
+            M_v_k = torch.cat((H_v[bmg.a2a], bmg.X_e[bmg.a2b]), 2)  # V x b x (d_h + d_e)
             M_v = M_v_k.sum(1)  # V x d_h + d_e
 
             H_v = H_0 + self.W_h(M_v)  # E x d_h
             H_v = self.act(H_v)
             H_v = self.dropout(H_v)
 
-        M_v_k = H_v[a2a]
+        M_v_k = H_v[bmg.a2a]
         M_v = M_v_k.sum(1)  # V x d_h
 
-        H_v = self.act(self.W_o(torch.cat((X_v, M_v), 1)))  # V x d_h
+        H_v = self.act(self.W_o(torch.cat((bmg.X_v, M_v), 1)))  # V x d_h
         H_v = self.dropout(H_v)
 
         if X_vd is not None:
             H_v = self.cat_descriptors(H_v, X_vd)
 
-        H = self.readout(H_v[1:], [n_a for _, n_a in a_scope])  # B x d_h (+ d_vd)
+        H = self.readout(H_v[1:], [n_a for _, n_a in bmg.a_scope])  # B x d_h (+ d_vd)
 
         return H
 
