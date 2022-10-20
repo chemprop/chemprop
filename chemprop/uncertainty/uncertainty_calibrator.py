Changed all code to fix masking. Changed data generation to include gaps

chemprop/uncertainty/uncertainty_calibrator.py
@@ -773,9 +773,11 @@ class ConformalMulticlassCalibrator(UncertaintyCalibrator):
         uncal_preds = np.array(
             self.calibration_predictor.get_uncal_preds()
         )  # shape(data, tasks, num_classes)
-        targets = np.array(self.calibration_data.targets(), dtype=int)  # shape(data, tasks)
-        mask = ~np.isnan(targets)
-        targets = np.nan_to_num(targets)
+        targets = np.array(self.calibration_data.targets(), dtype=float)  # shape(data, tasks)
+        print(targets)
+        #mask = ~np.isnan(targets)
+        mask = np.array(self.calibration_data.mask(), dtype=bool)
+        #targets = np.nan_to_num(targets)
         num_data, self.num_tasks, self.num_classes = uncal_preds.shape[:3]
 
         all_scores = self.nonconformity_scores(uncal_preds)
@@ -783,9 +785,9 @@ class ConformalMulticlassCalibrator(UncertaintyCalibrator):
 
         for task_id in range(self.num_tasks):
             task_scores = np.take_along_axis(
-                all_scores[:, task_id], targets[:, task_id].reshape(-1, 1), axis=1
-            ).squeeze(1)
-            task_scores = task_scores[mask[:, task_id]]  # shape(valid_data)
+                all_scores[mask[:, task_id], task_id], targets[mask[:, task_id], task_id].reshape(-1, 1).astype(int), axis=1
+            ).squeeze(1) # shape(valid_data)
+            #task_scores = task_scores[mask[:, task_id]]  # shape(valid_data)
             task_scores = np.append(task_scores, np.inf)
             task_scores = np.sort(task_scores)
             qhat = np.quantile(task_scores, 1 - self.conformal_alpha / self.num_tasks, method="higher")
@@ -951,8 +953,8 @@ class ConformalRegressionCalibrator(UncertaintyCalibrator):
     def calibrate(self):
         uncal_interval = self.get_interval(self.calibration_predictor)
         targets = np.array(self.calibration_data.targets(), dtype=float)  # shape(data, tasks)
-        targets = np.nan_to_num(targets)
-        mask = ~np.isnan(targets)
+        #targets = np.nan_to_num(targets)
+        mask = np.array(self.calibration_data.mask(), dtype=bool)
 
         self.num_tasks = uncal_interval.shape[1] // 2
         self.qhats = []
@@ -960,8 +962,8 @@ class ConformalRegressionCalibrator(UncertaintyCalibrator):
         for task_id in range(self.num_tasks):
             targets_task_id = targets[:, task_id]
             targets_task_id = targets_task_id[mask[:, task_id]]
-            uncal_interval_lower = uncal_interval[:, task_id]
-            uncal_interval_upper = uncal_interval[:, task_id + self.num_tasks]
+            uncal_interval_lower = uncal_interval[mask[:, task_id], task_id]
+            uncal_interval_upper = uncal_interval[mask[:, task_id], task_id + self.num_tasks]
 
             calibration_scores = np.maximum(
                 uncal_interval_lower - targets_task_id, targets_task_id - uncal_interval_upper

chemprop/uncertainty/uncertainty_evaluator.py
@@ -417,15 +417,26 @@ class ConformalRegressionEvaluator(UncertaintyEvaluator):
         uncertainties: List[List[float]], # shape (data, 2*tasks)
         mask: List[List[bool]],
     ):
+        """
+        Args:
+            targets: shape(data, tasks)
+            preds: shape(data, tasks, num_classes)
+            uncertainties: shape(data, 2*tasks, num_classes)
+            mask: shape(data, tasks)
+
+        Returns:
+            Conformal coverage for each task
+        """
         targets = np.array(targets)
+        mask = np.array(mask, dtype=bool)
         uncertainties = np.array(uncertainties)
-        num_tasks = targets.shape[1]
+        num_tasks = uncertainties.shape[1]//2
         results = []
 
         for task_id in range(num_tasks):
-            unc_task_id_lower = uncertainties[:, task_id]
-            unc_task_id_upper = uncertainties[:, task_id + num_tasks]
-            targets_task_id = targets[:, task_id]
+            unc_task_id_lower = uncertainties[mask[:, task_id], task_id]
+            unc_task_id_upper = uncertainties[mask[:, task_id], task_id + num_tasks]
+            targets_task_id = targets[mask[:, task_id], task_id]
             task_results = np.logical_and(unc_task_id_lower <= targets_task_id, targets_task_id <= unc_task_id_upper)
             results.append(task_results.sum() / task_results.shape[0])
 
@@ -461,14 +472,15 @@ class ConformalMulticlassEvaluator(UncertaintyEvaluator):
         Returns:
             Conformal coverage for each task
         """
-        targets = np.array(targets, dtype=int)
+        targets = np.array(targets, dtype=float)
+        mask = np.array(mask, dtype=bool)
         uncertainties = np.array(uncertainties)
         num_tasks = targets.shape[1]
         results = []
 
         for task_id in range(num_tasks):
             task_results = np.take_along_axis(
-                uncertainties[:, task_id], targets[:, task_id].reshape(-1, 1), axis=1
+                uncertainties[mask[:, task_id], task_id], targets[mask[:, task_id], task_id].reshape(-1, 1).astype(int), axis=1
             ).squeeze(1)
             results.append(task_results.sum() / task_results.shape[0])
 