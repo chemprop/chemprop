a0299176e91d2f1b87787cb7f0ca11e23f02b40c
shihchengli <scli@mit.edu>
2023-03-09 14:04:59 +0800

    Avoid OverflowError when setting field size to sys.maxsize

--- chemprop/data/utils.py
+++ chemprop/data/utils.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict, defaultdict
 import sys
 import csv
+import ctypes
 from logging import Logger
 import pickle
 from random import Random
@@ -19,8 +20,8 @@ from chemprop.args import PredictArgs, TrainArgs
 from chemprop.features import load_features, load_valid_atom_or_bond_features, is_mol
 from chemprop.rdkit import make_mol
 
-# Increase maximum size of field in the csv processing
-csv.field_size_limit(sys.maxsize)
+# Increase maximum size of field in the csv processing for the current architecture
+csv.field_size_limit(int(ctypes.c_ulong(-1).value // 2))
 
 def get_header(path: str) -> List[str]:
     """
