be93e94b040a4d5be490dd7659c01e63b6eb387e
david graff <deg711@g.harvard.edu>
2023-05-22 15:00:35 -0400

    documenting molgraphs

--- chemprop/v2/featurizers/molgraph.py
+++ chemprop/v2/featurizers/molgraph.py
@@ -1,50 +1,33 @@
 from __future__ import annotations
-from ctypes import Union
 
-from dataclasses import InitVar, dataclass, field
-from typing import Iterable, NamedTuple, Optional
+from typing import NamedTuple, Sequence
 
 import numpy as np
 import torch
-from torch import Tensor
 
 
 class MolGraph(NamedTuple):
-    """A `MolGraph` represents the graph structure and featurization of a single molecule.
-
-    Attributes
-    ----------
-    n_atoms : int
-        the number of atoms in the molecule
-    n_bonds : int
-        the number of bonds in the molecule
-    X_v : np.ndarray
-        an array of shape `V x d_v` containing the atom features of the molecule
-    X_e : np.ndarray
-        an array of shape `E x d_e` containing the bond features of the molecule
-    a2b : list[tuple[int]]
-        A list of length `V` that maps from an atom index to a list of incoming bond indices.
-    b2a : np.ndarray
-        A list of length `E` that maps from a bond index to the index of the atom the bond
-        originates from.
-    b2revb : np.ndarray
-        A list of length `E` that maps from a bond index to the index of the reverse bond.
-    a2a : Optional[list[int]]
-    b2b: Optional[np.ndarray]
-    """
+    """A `MolGraph` represents the graph featurization of a molecule."""
 
     n_atoms: int
+    """the number of atoms in the molecule"""
     n_bonds: int
-    X_v: np.ndarray
-    X_e: np.ndarray
+    """the number of bonds in the molecule"""
+    V: np.ndarray
+    """an array of shape `V x d_v` containing the atom features of the molecule"""
+    E: np.ndarray
+    """an array of shape `E x d_e` containing the bond features of the molecule"""
     a2b: list[tuple[int]]
+    """A list of length `V` that maps from an atom index to a list of incoming bond indices."""
     b2a: list[int]
+    """A list of length `E` that maps from a bond index to the index of the atom the bond
+    originates from."""
     b2revb: np.ndarray
-    a2a: Optional[list[int]]
-    b2b: Optional[np.ndarray]
+    """A list of length `E` that maps from a bond index to the index of the reverse bond."""
+    a2a: list[int] | None
+    b2b: np.ndarray | None
 
 
-@dataclass
 class BatchMolGraph:
     """A `BatchMolGraph` represents a batch of individual `MolGraph`s.
 
@@ -59,69 +42,56 @@ class BatchMolGraph:
     Attributes
     ----------
     n_atoms : int
+        the number of atoms in the batched graph
     n_bonds : int
-    X_v : Tensor
-    X_e : Tensor
+        the number of bonds in the batched graph
+    V : Tensor
+        the atom feature matrix
+    E : Tensor
+        the bond feature matrix
     a2b : Tensor
+        a mapping from atom index to indices of incoming bonds 
     b2a : Tensor
+        a mapping from bond index to index of the originating atom
     b2revb : Tensor
+        A mapping from bond index to the index of the reverse bond.
     a2a : Tensor
-    a_scope : list[tuple[int]]
-        a list of tuples containing `(start_index, n_atoms)` for each molecule in the batch
-    b_scope : list[tuple[int]]
-        a list of tuples containing `(start_index, n_bonds)` for each molecule in the batch
+        a mapping from atom index to the indices of connected atoms
+    a_scope : list[int]
+        the number of atoms for each molecule in the batch
+    b_scope : list[int]
+        the number of bonds for each molecule in the batch
     """
 
-    mgs: InitVar[Iterable[MolGraph]]
-    n_atoms: int = field(init=False)
-    n_bonds: int = field(init=False)
-    X_v: Tensor = field(init=False)
-    """the atom feature matrix"""
-    X_e: Tensor = field(init=False)
-    """the bond feature matrix"""
-    a2b: Tensor = field(init=False)
-    """a mapping from atom index to indices of incoming bonds"""
-    b2a: Tensor = field(init=False)
-    """a mapping from bond index to index of the originating atom"""
-    b2revb: Tensor = field(init=False)
-    """a mapping from bond index to the index of the reverse bond"""
-    a2a: Tensor = field(init=False)
-    """a mapping from atom index to the indices of connected atoms"""
-    # b2b: Optional[np.ndarray] = field(init=False)
-    a_scope: list[tuple[int, int]] = field(init=False)
-    """a list of tuples containing `(start_index, n_atoms)` for each molecule in the batch"""
-    b_scope: list[tuple[int, int]] = field(init=False)
-    """a list of tuples containing `(start_index, n_bonds)` for each molecule in the batch"""
-
-    def __post_init__(self, mgs: Iterable[MolGraph]):
+    def __init__(self, mgs: Sequence[MolGraph]):
         self.n_atoms = 1
         self.n_bonds = 1
         self.a_scope = []
         self.b_scope = []
 
-        # All start with zero padding so that indexing with zero padding returns zeros
-        X_vs = [np.zeros((1, mgs[0].X_v.shape[1]))]
-        X_es = [np.zeros((1, mgs[0].X_e.shape[1]))]
+        # All start with zero padding so that padding indices with zeros returns a zero vector
+        Vs = [np.zeros((1, mgs[0].V.shape[1]))]
+        Es = [np.zeros((1, mgs[0].E.shape[1]))]
         a2b = [[]]
         b2a = [0]
         b2revb = [0]
 
         for mg in mgs:
-            X_vs.append(mg.X_v)
-            X_es.append(mg.X_e)
+            Vs.append(mg.V)
+            Es.append(mg.E)
 
             a2b.extend([b + self.n_bonds for b in mg.a2b[a]] for a in range(mg.n_atoms))
             b2a.extend(self.n_atoms + mg.b2a[b] for b in range(mg.n_bonds))
             b2revb.extend(self.n_bonds + mg.b2revb[b] for b in range(mg.n_bonds))
 
-            self.a_scope.append((self.n_atoms, mg.n_atoms))
-            self.b_scope.append((self.n_bonds, mg.n_bonds))
+            self.a_scope.append(mg.n_atoms)
+            self.b_scope.append(mg.n_bonds)
 
             self.n_atoms += mg.n_atoms
             self.n_bonds += mg.n_bonds
 
-        self.X_v = torch.from_numpy(np.concatenate(X_vs)).float()
-        self.X_e = torch.from_numpy(np.concatenate(X_es)).float()
+        self.V = torch.from_numpy(np.concatenate(Vs)).float()
+        self.E = torch.from_numpy(np.concatenate(Es)).float()
 
         # max with 1 to fix a crash in rare case of all single-heavy-atom mols
         max_num_bonds = max(1, max(len(in_bonds) for in_bonds in a2b))
@@ -134,11 +104,12 @@ class BatchMolGraph:
         self.a2a = self.b2a[self.a2b]
 
     def __len__(self) -> int:
+        """the number of individual `MolGraph`s in this batch"""
         return len(self.a_scope)
 
-    def to(self, device: Union[str, torch.device]):
-        self.X_v = self.X_v.to(device)
-        self.X_e = self.X_e.to(device)
+    def to(self, device: str | torch.device):
+        self.V = self.V.to(device)
+        self.E = self.E.to(device)
         self.a2b = self.a2b.to(device)
         self.b2a = self.b2a.to(device)
         self.b2revb = self.b2revb.to(device)
