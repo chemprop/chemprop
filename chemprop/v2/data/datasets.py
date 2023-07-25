def9ed791616ff7b518b21cfea0214b8dfa38081
david graff <deg711@g.harvard.edu>
2023-07-25 09:26:35 -0400

    update naming

--- chemprop/v2/data/datasets.py
+++ chemprop/v2/data/datasets.py
@@ -111,15 +111,29 @@ class MolGraphDatasetBase(Dataset):
         StandardScaler
             a scaler fit to the targets.
         """
-        targets = np.array([d._y for d in self.data])
-        scaler = (scaler or StandardScaler()).fit(targets)
-        scaled_targets = scaler.transform(targets)
-        self.Y = scaled_targets
+        Y = np.array([d._y for d in self.data])
+        
+        scaler = (scaler or StandardScaler()).fit(Y)
+        self.Y = scaler.transform(Y)
+
+        return scaler
+    
+    def normalize_inputs(
+        self, key: str | None = "X_v", scaler: StandardScaler | None = None
+    ) -> StandardScaler:
+        VALID_KEYS = {"X_v", None}
+        if key not in VALID_KEYS:
+            raise ValueError(f"Invalid feature key! got: {key}. expected one of: {VALID_KEYS}")
+
+        X = self.X_v
+
+        if scaler is None:
+            scaler = StandardScaler().fit(X)
 
         return scaler
 
     def reset(self) -> None:
-        """Resets the features (atom, bond, and molecule) and targets of each datapoint to its 
+        """Reset the {atom, bond, molecule} features and targets of each datapoint to its 
         initial, unnormalized values."""
         [d.reset() for d in self.data]
 
@@ -167,54 +181,54 @@ class MoleculeDataset(MolGraphDatasetBase):
         return 1
 
     @property
-    def V_f(self) -> list[np.ndarray] | None:
+    def V_fs(self) -> list[np.ndarray] | None:
         if len(self.data) > 0 and self.data[0].V_f is None:
             return None
 
         return [d.V_f for d in self.data]
 
-    @V_f.setter
-    def V_f(self, X: np.ndarray):
-        if not len(self.data) == len(X):
+    @V_fs.setter
+    def V_fs(self, V_fs: list[np.ndarray]):
+        if not len(self.data) == len(V_fs):
             raise ValueError(
-                "number of molecules and atom features must be of same length! "
-                f"expected: `{len(self.data)} x *`. got: `{len(X)} x *`"
+                "number of molecules and supplied atom features must be of same length! "
+                f"expected: {len(self.data)}. got: {len(V_fs)}"
             )
-        for d, x in zip(self.data, X):
-            d.V_f = x
+        for d, V_f in zip(self.data, V_fs):
+            d.V_f = V_f
 
     @property
-    def E_f(self) -> list[np.ndarray] | None:
+    def E_fs(self) -> list[np.ndarray] | None:
         if len(self.data) > 0 and self.data[0].E_f is None:
             return None
 
         return [d.E_f for d in self.data]
 
-    @E_f.setter
-    def E_f(self, X: np.ndarray):
-        if not len(self.data) == len(X):
+    @E_fs.setter
+    def E_fs(self, E_fs: list[np.ndarray]):
+        if not len(self.data) == len(E_fs):
             raise ValueError(
-                "number of molecules and edge features must be of same length! "
-                f"expected: `{len(self.data)} x *`. got: `{len(X)} x *`"
+                "number of molecules and supplied edge features must be of same length! "
+                f"expected: {len(self.data)}. got: {len(E_fs)}"
             )
-        for d, E_f in zip(self.data, X):
+        for d, E_f in zip(self.data, E_fs):
             d.V_d = E_f
 
     @property
-    def V_d(self) -> list[np.ndarray] | None:
+    def V_ds(self) -> list[np.ndarray] | None:
         if len(self.data) > 0 and self.data[0].V_d is None:
             return None
 
         return [d.V_d for d in self.data]
 
-    @V_d.setter
-    def V_d(self, X: np.ndarray):
-        if not len(self.data) == len(X):
+    @V_ds.setter
+    def V_ds(self, V_ds: list[np.ndarray]):
+        if not len(self.data) == len(V_ds):
             raise ValueError(
-                "number of molecules and atom descriptors must be of same length! "
-                f"expected: `{len(self.data)} x *`. got: `{len(X)} x *`"
+                "number of molecules and supplied atom descriptors must be of same length! "
+                f"expected: {len(self.data)}. got: {len(V_ds)}"
             )
-        for d, V_d in zip(self.data, X):
+        for d, V_d in zip(self.data, V_ds):
             d.V_d = V_d
 
     @property
@@ -238,39 +252,38 @@ class MoleculeDataset(MolGraphDatasetBase):
 
         return len(self.data[0].V_d[0])
 
-    def normalize(
+    def normalize_inputs(
         self, key: str | None = "X_v", scaler: StandardScaler | None = None
     ) -> StandardScaler:
-        VALID_KEYS = {"X_v", "V_f", "E_f", "V_d", "all", None}
+        VALID_KEYS = {"X_v", "V_f", "E_f", "V_d", None}
         if key not in VALID_KEYS:
             raise ValueError(f"Invalid feature key! got: {key}. expected one of: {VALID_KEYS}")
 
         if key == "X_v":
             X = self.X_v
         elif key == "V_f":
-            X = self.V_f
+            X = None if self.V_fs is None else np.concatenate(self.V_fs, axis=0)
         elif key == "E_f":
-            X = self.E_f
+            X = None if self.E_fs is None else np.concatenate(self.E_fs, axis=0)
         elif key == "V_d":
-            X = self.V_d
-        elif key is None or key == "all":
-            return [self.normalize(k, scaler) for k in VALID_KEYS - {None, "all"}]
+            X = None if self.V_ds is None else np.concatenate(self.V_ds, axis=0)
+        elif key is None:
+            return [self.normalize_inputs(k, scaler) for k in VALID_KEYS - {None}]
 
         if X is None:
             return scaler
 
         if scaler is None:
             scaler = StandardScaler().fit(X)
-        X_ = scaler.transform(X)
 
         if key == "X_v":
-            self.X_v = X_
+            self.X_v = scaler.transform(X)
         elif key == "V_f":
-            self.V_f = X_
+            self.V_fs = [scaler.transform(V_f) for V_f in self.V_fs]
         elif key == "E_f":
-            self.E_f = X_
+            self.E_fs = [scaler.transform(E_f) for E_f in self.E_fs]
         elif key == "V_d":
-            self.E_f = X_
+            self.V_ds = [scaler.transform(V_d) for V_d in self.V_ds]
 
         return scaler
 
