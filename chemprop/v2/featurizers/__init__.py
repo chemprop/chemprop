5198c2e46d9a34884f9c95c3084d2bfec1274c10
Angiras Menon <am2145@github.com>
2023-09-25 12:17:22 -0400

    Renaming RxnMolGraphFeaturizer to CGRFeaturizer

--- chemprop/v2/featurizers/__init__.py
+++ chemprop/v2/featurizers/__init__.py
@@ -3,4 +3,4 @@ from .atom import AtomFeaturizer
 from .bond import BondFeaturizer
 from .molgraph import MolGraph, BatchMolGraph
 from .molecule import MoleculeMolGraphFeaturizer
-from .reaction import RxnMolGraphFeaturizer, RxnMode
+from .reaction import CGRFeaturizer, RxnMode
