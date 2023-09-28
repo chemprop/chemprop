4c63881650fdb308012add5f8bc1cecc876e2451
Nathan Morgan <nate.k.morgan@gmail.com>
2023-09-28 16:56:43 -0400

    Add Smiles Columns and Checkpoint Dir functions

--- chemprop/v2/cli/utils/__init__.py
+++ chemprop/v2/cli/utils/__init__.py
@@ -1,5 +1,6 @@
 from .args import bounded
 from .actions import RegistryAction
 from .command import Subcommand
-from .config import LOG_DIR, NOW
+from .config import CHK_DIR, LOG_DIR, NOW
 from .utils import *
+from .utils import column_str_to_int # Strangely this wasn't included in the glob above for me (Knathan)
