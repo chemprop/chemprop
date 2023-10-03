9e094656521bf35bc10ccefe61bc3762a6f4f386
am2145 <angirasmenon@gmail.com>
2023-10-03 15:08:43 -0400

    This commit moves the test_dataloader.py into the tests folder within v2 to have it located with the other v2 tests. Additionally, the tests itself have been updated to use assert_close from torch.testing and remove a duplicate isinstance assertion test as suggested by David. The weight in Datum is now passed as a float for the comparison, and lt_mask and gt_mask values have been changed to be Booleans in line with other sections of the v2 code.

--- chemprop/v2/tests/unit/data/test_dataloader.py
+++ chemprop/v2/tests/unit/data/test_dataloader.py
@@ -21,12 +21,12 @@ def datum_1():
     )
     return Datum(
         mol_graph1,
-        V_d = np.array([1.0, 2.0]), 
-        x_f = [3, 4], 
-        y = [6, 7], 
-        weight = [8], 
-        lt_mask = [9], 
-        gt_mask= [10]
+        V_d = np.array([1.0, 2.0]),
+        x_f = [3, 4],
+        y = [6, 7],
+        weight = [8.0],
+        lt_mask = [True],
+        gt_mask= [False]
     )
 
 @pytest.fixture
@@ -44,46 +44,45 @@ def datum_2():
     )
     return Datum(
         mol_graph2,
-        V_d = np.array([5.0, 7.0]), 
-        x_f = [8, 9], 
-        y = [6, 4], 
-        weight = [1], 
-        lt_mask = [2], 
-        gt_mask= [3]
+        V_d = np.array([5.0, 7.0]),
+        x_f = [8, 9],
+        y = [6, 4],
+        weight = [1.0],
+        lt_mask = [False],
+        gt_mask= [True]
     )
 
 
 def test_collate_batch_single_graph(datum_1):
-    
+
     batch = [
         datum_1,
     ]
-    
+
     result = collate_batch(batch)
     mgs, V_ds, x_fs, ys, weights, lt_masks, gt_masks = result
-    
+
     assert isinstance(result, tuple)
     assert isinstance(mgs, BatchMolGraph)
-    assert torch.equal(V_ds, torch.tensor([1.0, 2.0], dtype=torch.float32))
-    assert torch.equal(x_fs, torch.tensor([[3.0, 4.0]], dtype=torch.float32))
-    assert torch.equal(ys, torch.tensor([[6.0, 7.0]], dtype=torch.float32))
-    assert torch.equal(weights, torch.tensor([[[8.0]]], dtype=torch.float32))
-    assert torch.equal(lt_masks, torch.tensor([[9.0]], dtype=torch.float32))
-    assert torch.equal(gt_masks, torch.tensor([[10.0]], dtype=torch.float32))
+    torch.testing.assert_close(V_ds, torch.tensor([1.0, 2.0], dtype=torch.float32))
+    torch.testing.assert_close(x_fs, torch.tensor([[3.0, 4.0]], dtype=torch.float32))
+    torch.testing.assert_close(ys, torch.tensor([[6.0, 7.0]], dtype=torch.float32))
+    torch.testing.assert_close(weights, torch.tensor([[[8.0]]], dtype=torch.float32))
+    torch.testing.assert_close(lt_masks, torch.tensor([[1]], dtype=torch.bool))
+    torch.testing.assert_close(gt_masks, torch.tensor([[0]], dtype=torch.bool))
 
 
 def test_collate_batch_multiple_graphs(datum_1, datum_2):
-    
+
     batch = [datum_1, datum_2]
-    
+
     result = collate_batch(batch)
     mgs, V_ds, x_fs, ys, weights, lt_masks, gt_masks = result
-    
-    assert isinstance(result, tuple)
+
     assert isinstance(mgs, BatchMolGraph)
-    assert torch.equal(V_ds, torch.tensor([1.0, 2.0, 5.0, 7.0], dtype=torch.float32))
-    assert torch.equal(x_fs, torch.tensor([[3.0, 4.0], [8.0, 9.0]], dtype=torch.float32))
-    assert torch.equal(ys, torch.tensor([[6.0, 7.0], [6.0, 4.0]], dtype=torch.float32))
-    assert torch.equal(weights, torch.tensor([[[8.0]], [[1.0]]], dtype=torch.float32))
-    assert torch.equal(lt_masks, torch.tensor([[9.0], [2.0]], dtype=torch.float32)) 
-    assert torch.equal(gt_masks, torch.tensor([[10.0], [3.0]], dtype=torch.float32)) 
+    torch.testing.assert_close(V_ds, torch.tensor([1.0, 2.0, 5.0, 7.0], dtype=torch.float32))
+    torch.testing.assert_close(x_fs, torch.tensor([[3.0, 4.0], [8.0, 9.0]], dtype=torch.float32))
+    torch.testing.assert_close(ys, torch.tensor([[6.0, 7.0], [6.0, 4.0]], dtype=torch.float32))
+    torch.testing.assert_close(weights, torch.tensor([[[8.0]], [[1.0]]], dtype=torch.float32))
+    torch.testing.assert_close(lt_masks, torch.tensor([[1], [0]], dtype=torch.bool))
+    torch.testing.assert_close(gt_masks, torch.tensor([[0], [1]], dtype=torch.bool))
