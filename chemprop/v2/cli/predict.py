57e111a888d5f01cb119440e6bcee57e544d9b76
Kevin Greenman <kpg@mit.edu>
2023-10-04 18:16:42 -0400

    flatten preds + TODO

--- chemprop/v2/cli/predict.py
+++ chemprop/v2/cli/predict.py
@@ -245,7 +245,7 @@ def main(args):
     # TODO: might want to write a shared function for this as train.py might also want to do this.
     df_test = pd.read_csv(args.test_path)
     preds = torch.concat(predss, 1).numpy()
-    df_test["preds"] = preds
+    df_test["preds"] = preds.flatten()  # TODO: this will not work correctly for multi-target predictions
     if args.output.suffix == ".pkl":
         df_test.to_pickle(args.output, index=False)
     else:
