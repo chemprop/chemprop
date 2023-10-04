89fa1755b842983b3622b9944c32f46565420cd3
Kevin Greenman <35846516+kevingreenman@users.noreply.github.com>
2023-10-04 16:45:54 -0400

    chk to ckpt
    Co-authored-by: david graff <60193893+davidegraff@users.noreply.github.com>

--- chemprop/v2/cli/utils/config.py
+++ chemprop/v2/cli/utils/config.py
@@ -2,6 +2,6 @@ from datetime import datetime
 import os
 from pathlib import Path
 
-CHK_DIR = Path(os.getenv("CHEMPROP_CHK_DIR", "model_checkpoints"))
+CKPT_DIR = Path(os.getenv("CHEMPROP_CKPT_DIR", "model_checkpoints"))
 LOG_DIR = Path(os.getenv("CHEMPROP_LOG_DIR", "logs/chemprop"))
 NOW = datetime.now().isoformat("T", "seconds")
