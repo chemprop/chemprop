500133dbfce236ceb364d187aa27ed02660a1aaf
Hao-Wei Pang <hwpang@mit.edu>
2023-10-06 18:27:39 -0400

    Add MulticomponentDatapoint to init

--- chemprop/v2/data/__init__.py
+++ chemprop/v2/data/__init__.py
@@ -1,4 +1,4 @@
 from .dataloader import MolGraphDataLoader
-from .datapoints import MoleculeDatapoint, ReactionDatapoint
+from .datapoints import MoleculeDatapoint, ReactionDatapoint, MulticomponentDatapoint
 from .datasets import _MolGraphDatasetMixin, MoleculeDataset, ReactionDataset
 from .samplers import ClassBalanceSampler, SeededSampler
