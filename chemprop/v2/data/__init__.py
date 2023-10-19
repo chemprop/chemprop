7260a39d53dc89d2a68e6e5160b6a3bb60d2dbe1
hwpang <hwpang@mit.edu>
2023-10-19 19:07:42 -0400

    import split type

--- chemprop/v2/data/__init__.py
+++ chemprop/v2/data/__init__.py
@@ -2,3 +2,4 @@ from .dataloader import MolGraphDataLoader
 from .datapoints import MoleculeDatapoint, ReactionDatapoint
 from .datasets import _MolGraphDatasetMixin, MoleculeDataset, ReactionDataset, MulticomponentDataset
 from .samplers import ClassBalanceSampler, SeededSampler
+from .utils import split_data, SplitType
