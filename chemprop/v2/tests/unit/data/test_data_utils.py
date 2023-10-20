714641b02a9c992a666e1b583db44048fdddec6a
Jackson Burns <jwburns@mit.edu>
2023-10-20 12:49:29 -0400

    black format + remove some code churn in tests for split_data

--- chemprop/v2/tests/unit/data/test_data_utils.py
+++ chemprop/v2/tests/unit/data/test_data_utils.py
@@ -9,6 +9,14 @@ from chemprop.v2.data.datapoints import MoleculeDatapoint
 from chemprop.v2.data.utils import split_data, _unpack_astartes_result
 
 
+def split_fail(split_type):
+    return (
+        f"Data splitting with type {split_type} has failed. "
+        "The failure is likely due to a change of the corresponding data splitting backend. "
+        "Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
+    )
+
+
 @pytest.fixture
 def molecule_dataset():
     """A dataset with single molecules"""
@@ -34,13 +42,14 @@ def test_splits_sum1_warning(molecule_dataset):
     """Testing that the splits are normalized to 1, for overspecified case."""
     with pytest.warns(NormalizationWarning):
         split_data(datapoints=molecule_dataset, sizes=(0.4, 0.6, 0.2))
-        
+
+
 def test_splits_sum2_warning(molecule_dataset):
     """Testing that the splits are normalized to 1, for underspecified case."""
     with pytest.warns(NormalizationWarning):
         split_data(datapoints=molecule_dataset, sizes=(0.1, 0.1, 0.1))
-        
-        
+
+
 def test_three_splits_provided(molecule_dataset):
     """Testing that three splits are provided"""
     with pytest.raises(RuntimeError):
@@ -51,24 +60,25 @@ def test_seed0(molecule_dataset):
     """
     Testing that split_data can get expected output using astartes as backend for random split with seed 0.
     Note: the behaviour of randomness for data splitting is not controlled by Chemprop but by the chosen backend.
-    """ 
+    """
     train, val, test = split_data(datapoints=molecule_dataset, seed=0)
-    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(molecule_dataset,
-                                                                          train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", random_state=0),
-                                                                          True)
+    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(
+        molecule_dataset, train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", random_state=0), True
+    )
     assert set([Chem.MolToSmiles(i.mol) for i in train]) == set([Chem.MolToSmiles(i.mol) for i in train_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in val]) == set([Chem.MolToSmiles(i.mol) for i in val_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in test]) == set([Chem.MolToSmiles(i.mol) for i in test_astartes])
-    
+
+
 def test_seed100(molecule_dataset):
     """
     Testing that split_data can get expected output using astartes as backend for random split with seed 100.
     Note: the behaviour of randomness for data splitting is not controlled by Chemprop but by the chosen backend.
-    """ 
+    """
     train, val, test = split_data(datapoints=molecule_dataset, seed=100)
-    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(molecule_dataset,
-                                                                          train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", random_state=100),
-                                                                          True)
+    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(
+        molecule_dataset, train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", random_state=100), True
+    )
     assert set([Chem.MolToSmiles(i.mol) for i in train]) == set([Chem.MolToSmiles(i.mol) for i in train_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in val]) == set([Chem.MolToSmiles(i.mol) for i in val_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in test]) == set([Chem.MolToSmiles(i.mol) for i in test_astartes])
@@ -77,9 +87,11 @@ def test_seed100(molecule_dataset):
 def test_split_4_4_2(molecule_dataset):
     """Testing the random split with changed sizes"""
     train, val, test = split_data(datapoints=molecule_dataset, sizes=(0.4, 0.4, 0.2))
-    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(molecule_dataset,
-                                                                        train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", train_size=0.4, val_size=0.4, test_size=0.2, random_state=0),
-                                                                        True)
+    train_astartes, val_astartes, test_astartes = _unpack_astartes_result(
+        molecule_dataset,
+        train_val_test_split(np.arange(len(molecule_dataset)), sampler="random", train_size=0.4, val_size=0.4, test_size=0.2, random_state=0),
+        True,
+    )
     assert set([Chem.MolToSmiles(i.mol) for i in train]) == set([Chem.MolToSmiles(i.mol) for i in train_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in val]) == set([Chem.MolToSmiles(i.mol) for i in val_astartes])
     assert set([Chem.MolToSmiles(i.mol) for i in test]) == set([Chem.MolToSmiles(i.mol) for i in test_astartes])
@@ -98,9 +110,9 @@ def test_random_split(molecule_dataset_with_repeated_smiles):
     """
     split_type = "random"
     train, val, test = split_data(datapoints=molecule_dataset_with_repeated_smiles, sizes=(0.4, 0.4, 0.2), split=split_type)
-    
-    fail_msg = f"Data splitting with type {split_type} has failed. The failure is likely due to a change of the corresponding data splitting backend. Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
-    assert [Chem.MolToSmiles(i.mol) for i in train] == ['CN', 'CC'], fail_msg
+
+    assert [Chem.MolToSmiles(i.mol) for i in train] == ["CN", "CC"], split_fail(split_type)
+
 
 def test_repeated_smiles(molecule_dataset_with_repeated_smiles):
     """
@@ -109,10 +121,9 @@ def test_repeated_smiles(molecule_dataset_with_repeated_smiles):
     """
     split_type = "random_with_repeated_smiles"
     train, val, test = split_data(datapoints=molecule_dataset_with_repeated_smiles, sizes=(0.8, 0.0, 0.2), split=split_type)
-    
-    fail_msg = f"Data splitting with type {split_type} has failed. The failure is likely due to a change of the corresponding data splitting backend. Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
-    assert [Chem.MolToSmiles(i.mol) for i in train] == ["CO", "CC", "C", "C"], fail_msg
-    assert [Chem.MolToSmiles(i.mol) for i in test] == ["CN", "CN"], fail_msg
+
+    assert [Chem.MolToSmiles(i.mol) for i in train] == ["CO", "CC", "C", "C"], split_fail(split_type)
+    assert [Chem.MolToSmiles(i.mol) for i in test] == ["CN", "CN"], split_fail(split_type)
 
 
 def test_kennard_stone(molecule_dataset):
@@ -122,9 +133,8 @@ def test_kennard_stone(molecule_dataset):
     """
     split_type = "kennard_stone"
     train, val, test = split_data(datapoints=molecule_dataset, sizes=(0.4, 0.4, 0.2), split=split_type)
-    
-    fail_msg = f"Data splitting with type {split_type} has failed. The failure is likely due to a change of the corresponding data splitting backend. Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
-    assert set([Chem.MolToSmiles(i.mol) for i in test]) == set(["CCCO", "CCCN"]), fail_msg
+
+    assert set([Chem.MolToSmiles(i.mol) for i in test]) == set(["CCCO", "CCCN"]), split_fail(split_type)
 
 
 def test_kmeans(molecule_dataset):
@@ -134,9 +144,8 @@ def test_kmeans(molecule_dataset):
     """
     split_type = "kmeans"
     train, val, test = split_data(datapoints=molecule_dataset, sizes=(0.5, 0.0, 0.5), split=split_type)
-    
-    fail_msg = f"Data splitting with type {split_type} has failed. The failure is likely due to a change of the corresponding data splitting backend. Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
-    assert [Chem.MolToSmiles(i.mol) for i in train] == ["C", "CC", "CCC", "CN", "CO", "CCO", "CCCO"], fail_msg
+
+    assert [Chem.MolToSmiles(i.mol) for i in train] == ["C", "CC", "CCC", "CN", "CO", "CCO", "CCCO"], split_fail(split_type)
 
 
 def test_scaffold(molecule_dataset_with_rings):
@@ -146,6 +155,5 @@ def test_scaffold(molecule_dataset_with_rings):
     """
     split_type = "scaffold_balanced"
     train, val, test = split_data(datapoints=molecule_dataset_with_rings, sizes=(0.3, 0.3, 0.3), split=split_type)
-    
-    fail_msg = f"Data splitting with type {split_type} has failed. The failure is likely due to a change of the corresponding data splitting backend. Chemprop itself does not handle data splitting. The test here mainly serve as a red flag."
-    assert [Chem.MolToSmiles(i.mol) for i in train] == ['C', 'CC', 'CCC'], fail_msg
+
+    assert [Chem.MolToSmiles(i.mol) for i in train] == ["C", "CC", "CCC"], split_fail(split_type)
