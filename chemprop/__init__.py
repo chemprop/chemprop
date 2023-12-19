636160380cd5fd6ded4bd24441e67019b4168197
Kevin Greenman <kpg@mit.edu>
2023-12-19 17:23:09 -0500

    add top level init file

---
+++ chemprop/__init__.py
@@ -0,0 +1,13 @@
+import chemprop.cli
+import chemprop.data
+import chemprop.featurizers
+import chemprop.models
+import chemprop.nn
+import chemprop.utils
+
+import chemprop.conf
+import chemprop.exceptions
+import chemprop.metrics
+import chemprop.schedulers
+
+__version__ = '2.0.0b1'
