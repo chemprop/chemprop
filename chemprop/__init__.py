9e213313f45db3bc0d7b876446fa8e3c2533a89a
Kevin Greenman <kpg@mit.edu>
2023-12-19 18:12:53 -0500

    simplify imports more

--- chemprop/__init__.py
+++ chemprop/__init__.py
@@ -1,8 +1,3 @@
-from . import data, featurizers, models, nn, utils
-
-import chemprop.conf
-import chemprop.exceptions
-import chemprop.metrics
-import chemprop.schedulers
+from . import data, featurizers, models, nn, utils, conf, exceptions, metrics, schedulers
 
 __version__ = '2.0.0b1'
