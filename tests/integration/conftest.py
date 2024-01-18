0a27b50b5cc6effd287f76f2fc67b5eaa3f1db3d
hwpang <hwpang@mit.edu>
2024-01-18 12:13:57 -0500

    Add mcmpnn to session fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,6 +3,7 @@ import warnings
 import pytest
 
 from chemprop import models, nn
+from chemprop.models import multi
 
 warnings.filterwarnings("ignore", module=r"lightning.*", append=True)
 
@@ -13,3 +14,13 @@ def mpnn(request):
     ffn = nn.RegressionFFN()
 
     return models.MPNN(request.param, agg, ffn, True)
+
+
+@pytest.fixture(scope="session")
+def mcmpnn(request):
+    block, n_components = request.param
+    mcmp = nn.MulticomponentMessagePassing([block for _ in range(n_components)], n_components)
+    agg = nn.SumAggregation()
+    ffn = nn.RegressionFFN(input_dim=mcmp.output_dim,)
+
+    return multi.MulticomponentMPNN(mcmp, agg, ffn, True)
