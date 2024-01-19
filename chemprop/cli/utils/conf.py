cc6fa08b5e7b17ba3edd9f8d140f1f6ba8e957f3
Nathan Morgan <nate.k.morgan@gmail.com>
2024-01-18 19:21:17 -0500

    Fix - Use dash as separator

--- chemprop/cli/utils/conf.py
+++ chemprop/cli/utils/conf.py
@@ -4,5 +4,4 @@ from pathlib import Path
 
 CKPT_DIR = Path(os.getenv("CHEMPROP_CKPT_DIR", "model_checkpoints"))
 LOG_DIR = Path(os.getenv("CHEMPROP_LOG_DIR", "logs/chemprop"))
-NOW = datetime.now().isoformat("T", "seconds")
-NOW = datetime.datetime.strptime(NOW, "%Y-%m-%dT%H-%M-%S")
+NOW = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
