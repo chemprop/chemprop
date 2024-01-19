5a412fbdda2b2031bee81a1cc29783475f9394c2
Hao-Wei Pang <45482070+hwpang@users.noreply.github.com>
2024-01-19 11:36:21 -0500

    Update split key mol arg help string
    Co-authored-by: david graff <60193893+davidegraff@users.noreply.github.com>

--- chemprop/cli/train.py
+++ chemprop/cli/train.py
@@ -399,7 +399,7 @@ def add_train_args(parser: ArgumentParser) -> ArgumentParser:
         "--split-key-molecule",
         type=int,
         default=0,
-        help="The index of the key molecule used for splitting when multiple molecules are present and constrained split_type is used, like scaffold_balanced or random_with_repeated_smiles.       Note that this index begins with zero for the first molecule.",
+        help="The index of the key molecule used for splitting when multiple molecules are present and constrained split_type is used (e.g., 'scaffold_balanced' or 'random_with_repeated_smiles'). Note that this index begins with zero for the first molecule.",
     )
     split_args.add_argument(
         "-k",
