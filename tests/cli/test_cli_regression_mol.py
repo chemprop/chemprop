c4bbe98dea319c94d9290512be09a15de6e184a3
Hao-Wei Pang <hwpang@mit.edu>
2024-01-30 16:27:08 -0500

    Test expected output structure

--- tests/cli/test_cli_regression_mol.py
+++ tests/cli/test_cli_regression_mol.py
@@ -32,3 +32,29 @@ def test_predict_quick(monkeypatch, data_path, checkpoint_path):
     with monkeypatch.context() as m:
         m.setattr("sys.argv", args)
         main()
+
+
+def test_train_output_structure(monkeypatch, data_path, tmp_path):
+    args = ["chemprop", "train", "-i", data_path, "--epochs", "1", "--num-workers", "0", "--save-dir", tmp_path]
+
+    with monkeypatch.context() as m:
+        m.setattr("sys.argv", args)
+        main()
+
+    assert (tmp_path / "logs" / "chemprop").exists()
+    assert (tmp_path / "mol" / "model.pt").exists()
+    assert (tmp_path / "mol" / "chkpts" / "last.ckpt").exists()
+    assert (tmp_path / "mol" / "tb_logs" / "version_0").exists()
+
+
+def test_predict_output_structure(monkeypatch, data_path, tmp_path):
+    args = ["chemprop", "predict", "-i", data_path, "--checkpoint", "tests/data/example_model_v2.ckpt", "--output", tmp_path / "preds.csv"]
+
+    with monkeypatch.context() as m:
+        m.setattr("sys.argv", args)
+        main()
+
+    assert (tmp_path / "logs" / "chemprop").exists()
+    assert (tmp_path / "lightning_logs").exists()
+    assert (tmp_path / "preds.csv").exists()
+
