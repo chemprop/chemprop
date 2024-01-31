4b1de314d2767f85e4483970486701173109b244
Hao-Wei Pang <hwpang@mit.edu>
2024-01-31 15:32:23 -0500

    Rename to parser

--- chemprop/cli/train.py
+++ chemprop/cli/train.py
@@ -59,12 +59,12 @@ def add_train_args(parser: ArgumentParser) -> ArgumentParser:
     )
     # TODO: as we plug the three checkpoint options, see if we can reduce from three option to two or to just one.
     #        similar to how --features-path is/will be implemented
-    data_args.add_argument(
+    parser.add_argument(
         "--checkpoint-dir",
         help="Directory from which to load model checkpoints (walks directory and ensembles all models that are found).",
     )
-    data_args.add_argument("--checkpoint-path", help="Path to model checkpoint (:code:`.pt` file).")
-    data_args.add_argument(
+    parser.add_argument("--checkpoint-path", help="Path to model checkpoint (:code:`.pt` file).")
+    parser.add_argument(
         "--checkpoint-paths",
         type=list[str],
         help="List of paths to model checkpoints (:code:`.pt` files).",
