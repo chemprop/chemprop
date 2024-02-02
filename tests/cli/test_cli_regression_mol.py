51329a24347ba8e2f3ab04559a4812d4857a80b7
Hao-Wei Pang <hwpang@mit.edu>
2024-02-02 12:23:59 -0500

    Undo ensemble relevant changes

--- tests/cli/test_cli_regression_mol.py
+++ tests/cli/test_cli_regression_mol.py
@@ -43,8 +43,8 @@ def test_train_output_structure(monkeypatch, data_path, tmp_path):
         main()
 
     assert (tmp_path / "model.pt").exists()
-    assert (tmp_path / "model_0" / "chkpts" / "last.ckpt").exists()
-    assert (tmp_path / "model_0" / "tb_logs" / "lightning_logs" / "version_0").exists()
+    assert (tmp_path / "chkpts" / "last.ckpt").exists()
+    assert (tmp_path / "tb_logs" / "lightning_logs" / "version_0").exists()
 
 
 def test_predict_output_structure(monkeypatch, data_path, model_path, tmp_path):
@@ -64,6 +64,6 @@ def test_train_outputs(monkeypatch, data_path, tmp_path):
         m.setattr("sys.argv", args)
         main()
 
-    checkpoint_path = tmp_path / "model_0" / "chkpts" / "last.ckpt"
+    checkpoint_path = tmp_path / "chkpts" / "last.ckpt"
 
     model = MPNN.load_from_checkpoint(checkpoint_path)
