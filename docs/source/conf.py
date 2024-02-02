41315ffefb5e8cd5c1e4afa65f82b3b4d449c426
Anna Doner <donera@mit.edu>
2024-02-02 14:10:13 -0500

    fixed several docstrings that had characters directly following `, added autoapi_file_patterns to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
