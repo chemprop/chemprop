4f12a18f7ee6a4d5c636f6916ec94963d26da2ef
Hao-Wei Pang <hwpang@mit.edu>
2024-02-12 21:20:27 -0500

    dimension should be 1 x number of tasks

--- chemprop/cli/train.py
+++ chemprop/cli/train.py
@@ -762,8 +762,8 @@ def main(args):
 
         if test_loader is not None:
             if args.task_type == "regression":
-                model.predictor.register_buffer("loc", torch.tensor(scaler.mean_).view(-1, 1))
-                model.predictor.register_buffer("scale", torch.tensor(scaler.scale_).view(-1, 1))
+                model.predictor.register_buffer("loc", torch.tensor(scaler.mean_).view(1, -1))
+                model.predictor.register_buffer("scale", torch.tensor(scaler.scale_).view(1, -1))
             results = trainer.test(model, test_loader)[0]
             logger.info(f"Test results: {results}")
 
