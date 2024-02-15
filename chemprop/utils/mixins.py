23a65a150f1e258097876ea5b721c09ca029e07b
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-02-15 15:01:15 +0100

    fix wrong var names

--- chemprop/utils/mixins.py
+++ chemprop/utils/mixins.py
@@ -1,19 +1,26 @@
+"""Mixin classes for the project."""
+
 import inspect
 from typing import Any, Collection
 
 
 class ReprMixin:
     def __repr__(self) -> str:
-        """Return a string representation of the object."""
+        """Return a string representation of the object.
 
+        Returns
+        -------
+        str
+            The string representation of the object.
+        """
         param_repr_list = []
         for param_name, param_value in self.get_params(deep=False).items():
             param_default = self._get_default_params()[param_name]
             if param_value != param_default:
                 param_repr_list.append(f"{param_name}={repr(param_value)}")
 
-        argspec = ", ".join(param_repr_list)
-        return f"{self.__class__.__name__}({argspec})"
+        non_default_params = ", ".join(param_repr_list)
+        return f"{self.__class__.__name__}({non_default_params})"
 
     def _get_default_params(self) -> dict[str, Any]:
         """Get the default parameters for initializing the class."""
@@ -24,7 +31,7 @@ class ReprMixin:
         """Get the default parameters for initializing the class."""
         return sorted(self._get_default_params().keys())
 
-    def get_params(self, deep=True):
+    def get_params(self, deep=True) -> dict[str, Any]:
         """Get the current parameters of the class, required to initialize the class.
 
         This method mimics the behavior of `sklearn.base.BaseEstimator.get_params`.
@@ -37,16 +44,14 @@ class ReprMixin:
 
         Returns
         -------
-        params : dict
+        dict[str, Any]
             Parameter names mapped to their values.
         """
-        out = dict()
-        for param_name, _ in self._get_parm_names():
-            value = getattr(self, key)
+        out: dict[str, Any] = {}
+        for param_name in self._get_parm_names():
+            value = getattr(self, param_name)
             if deep and hasattr(value, "get_params") and not isinstance(value, type):
                 deep_items = value.get_params().items()
-                out.update((key + "__" + k, val) for k, val in deep_items)
-            out[key] = value
+                out.update((param_name + "__" + k, val) for k, val in deep_items)
+            out[param_name] = value
         return out
-
-
