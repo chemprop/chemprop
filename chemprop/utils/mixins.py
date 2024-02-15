52f5e8d22f4ddf34bc495de0c991a866ce122c59
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-02-15 14:55:46 +0100

    Rewrite get_params to comply with sklean formalism

--- chemprop/utils/mixins.py
+++ chemprop/utils/mixins.py
@@ -4,17 +4,49 @@ from typing import Any, Collection
 
 class ReprMixin:
     def __repr__(self) -> str:
-        items = self.get_params()
+        """Return a string representation of the object."""
 
-        if len(items) > 0:
-            keys, values = zip(*items)
-            sig = inspect.signature(self.__class__)
-            defaults = [sig.parameters[k].default for k in keys]
-            items = [(k, v) for k, v, d in zip(keys, values, defaults) if v != d]
+        param_repr_list = []
+        for param_name, param_value in self.get_params(deep=False).items():
+            param_default = self._get_default_params()[param_name]
+            if param_value != param_default:
+                param_repr_list.append(f"{param_name}={repr(param_value)}")
 
-        argspec = ", ".join(f"{k}={repr(v)}" for k, v in items)
-
+        argspec = ", ".join(param_repr_list)
         return f"{self.__class__.__name__}({argspec})"
 
-    def get_params(self) -> Collection[tuple[str, Any]]:
-        return self.__dict__.items()
+    def _get_default_params(self) -> dict[str, Any]:
+        """Get the default parameters for initializing the class."""
+        sig = inspect.signature(self.__class__)
+        return {k: v.default for k, v in sig.parameters.items()}
+
+    def _get_parm_names(self) -> list[str]:
+        """Get the default parameters for initializing the class."""
+        return sorted(self._get_default_params().keys())
+
+    def get_params(self, deep=True):
+        """Get the current parameters of the class, required to initialize the class.
+
+        This method mimics the behavior of `sklearn.base.BaseEstimator.get_params`.
+
+        Parameters
+        ----------
+        deep : bool, default=True
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+
+        Returns
+        -------
+        params : dict
+            Parameter names mapped to their values.
+        """
+        out = dict()
+        for param_name, _ in self._get_parm_names():
+            value = getattr(self, key)
+            if deep and hasattr(value, "get_params") and not isinstance(value, type):
+                deep_items = value.get_params().items()
+                out.update((key + "__" + k, val) for k, val in deep_items)
+            out[key] = value
+        return out
+
+
