912c8f3372efd2bc2505423137e8ddfd7fce149d
Hao-Wei Pang <hwpang@mit.edu>
2024-02-21 14:53:48 -0500

    Formatting

--- tests/cli/test_cli_regression_mol+mol.py
+++ tests/cli/test_cli_regression_mol+mol.py
@@ -19,7 +19,19 @@ def model_path(data_dir):
 
 
 def test_train_quick(monkeypatch, data_path):
-    args = ["chemprop", "train", "-i", data_path, "--smiles-columns", "smiles", "solvent", "--epochs", "1", "--num-workers", "0"]
+    args = [
+        "chemprop",
+        "train",
+        "-i",
+        data_path,
+        "--smiles-columns",
+        "smiles",
+        "solvent",
+        "--epochs",
+        "1",
+        "--num-workers",
+        "0",
+    ]
 
     with monkeypatch.context() as m:
         m.setattr("sys.argv", args)
@@ -27,14 +39,40 @@ def test_train_quick(monkeypatch, data_path):
 
 
 def test_predict_quick(monkeypatch, data_path, model_path):
-    args = ["chemprop", "predict", "-i", data_path, "--smiles-columns", "smiles", "solvent", "--model-path", model_path]
+    args = [
+        "chemprop",
+        "predict",
+        "-i",
+        data_path,
+        "--smiles-columns",
+        "smiles",
+        "solvent",
+        "--model-path",
+        model_path,
+    ]
 
     with monkeypatch.context() as m:
         m.setattr("sys.argv", args)
         main()
 
+
 def test_train_output_structure(monkeypatch, data_path, tmp_path):
-    args = ["chemprop", "train", "-i", data_path, "--smiles-columns", "smiles", "solvent", "--epochs", "1", "--num-workers", "0", "--save-dir", str(tmp_path), "--save-smiles-splits"]
+    args = [
+        "chemprop",
+        "train",
+        "-i",
+        data_path,
+        "--smiles-columns",
+        "smiles",
+        "solvent",
+        "--epochs",
+        "1",
+        "--num-workers",
+        "0",
+        "--save-dir",
+        str(tmp_path),
+        "--save-smiles-splits",
+    ]
 
     with monkeypatch.context() as m:
         m.setattr("sys.argv", args)
