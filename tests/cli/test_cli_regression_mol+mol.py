ce489f6fdc01912f5f8465e4b85bf2ed728d67da
Hao-Wei Pang <hwpang@mit.edu>
2024-02-21 09:06:06 -0500

    Change train smiles location

--- tests/cli/test_cli_regression_mol+mol.py
+++ tests/cli/test_cli_regression_mol+mol.py
@@ -43,4 +43,4 @@ def test_train_output_structure(monkeypatch, data_path, tmp_path):
     assert (tmp_path / "model_0" / "model.pt").exists()
     assert (tmp_path / "model_0" / "checkpoints" / "last.ckpt").exists()
     assert (tmp_path / "model_0" / "trainer_logs" / "version_0").exists()
-    assert (tmp_path / "model_0" / "train_smiles.csv").exists()
+    assert (tmp_path / "train_smiles.csv").exists()
