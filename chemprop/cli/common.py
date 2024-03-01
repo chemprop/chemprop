e6e49cdf6defc45401768ac4cef9528b393bff56
Hao-Wei Pang <hwpang@mit.edu>
2024-03-01 08:30:29 -0500

    import Path

--- chemprop/cli/common.py
+++ chemprop/cli/common.py
@@ -1,9 +1,10 @@
-from argparse import ArgumentParser, Namespace
 import logging
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 
 from chemprop.cli.utils import LookupAction
 from chemprop.cli.utils.args import uppercase
-from chemprop.featurizers import RxnMode, MoleculeFeaturizerRegistry
+from chemprop.featurizers import MoleculeFeaturizerRegistry, RxnMode
 
 logger = logging.getLogger(__name__)
 
