167a7c7d9b9d5ff4048a0f5ddfb1e4f0b1303877
Hao-Wei Pang <hwpang@mit.edu>
2024-03-01 15:27:48 -0500

    Reaction dataset doesn't have vf, ef, vd

--- chemprop/cli/train.py
+++ chemprop/cli/train.py
@@ -522,9 +522,9 @@ def normalize_inputs(train_dset, val_dset, args):
         )
     else:
         d_xd = train_dset.d_xd
-        d_vf = train_dset.d_vf
-        d_ef = train_dset.d_ef
-        d_vd = train_dset.d_vd
+        d_vf = train_dset.d_vf if not isinstance(train_dset, ReactionDataset) else 0
+        d_ef = train_dset.d_ef if not isinstance(train_dset, ReactionDataset) else 0
+        d_vd = train_dset.d_vd if not isinstance(train_dset, ReactionDataset) else 0
 
     if d_xd > 0 and not args.no_descriptor_scaling:
         X_d_scaler = train_dset.normalize_inputs("X_d")
