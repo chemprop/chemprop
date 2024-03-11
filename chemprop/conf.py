6670ad91ad3eaff4e614ec0925962f67464dced3
shihchengli <scli@mit.edu>
2024-03-11 18:37:47 -0400

    make the default AtomFeaturizer as MultiHotAtomFeaturizer.default

--- chemprop/conf.py
+++ chemprop/conf.py
@@ -1,6 +1,7 @@
 """Global configuration variables for chemprop"""
-from chemprop.featurizers.molgraph.molecule import SimpleMoleculeMolGraphFeaturizer
+
+from chemprop.featurizers import SimpleMoleculeMolGraphFeaturizer, MultiHotAtomFeaturizer
 
 
-DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = SimpleMoleculeMolGraphFeaturizer().shape
+DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = SimpleMoleculeMolGraphFeaturizer(atom_featurizer=MultiHotAtomFeaturizer.default()).shape
 DEFAULT_HIDDEN_DIM = 300
