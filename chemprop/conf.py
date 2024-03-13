f7c82ccaee315dc0dccb0845603703e6fb14e66a
Oscar Wu <oscarwu@mit.edu>
2024-03-13 15:51:19 -0400

    Fix: SimpleMoleculeMolGraphFeaturizer default call
    Default behavior for atom featurizer is set in mixins.py, so no need to specify here.

--- chemprop/conf.py
+++ chemprop/conf.py
@@ -1,7 +1,7 @@
 """Global configuration variables for chemprop"""
 
-from chemprop.featurizers import SimpleMoleculeMolGraphFeaturizer, MultiHotAtomFeaturizer
+from chemprop.featurizers import SimpleMoleculeMolGraphFeaturizer
 
 
-DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = SimpleMoleculeMolGraphFeaturizer(atom_featurizer=MultiHotAtomFeaturizer.default()).shape
+DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = SimpleMoleculeMolGraphFeaturizer().shape
 DEFAULT_HIDDEN_DIM = 300
