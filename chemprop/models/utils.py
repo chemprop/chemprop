0c0ccdab495db545a953b258eb50ebd817a71f94
Hao-Wei Pang <hwpang@mit.edu>
2024-04-01 11:32:28 -0400

    attach input scalers and output scaler under model

--- chemprop/models/utils.py
+++ chemprop/models/utils.py
@@ -2,7 +2,7 @@ from os import PathLike
 import torch
 from sklearn.preprocessing import StandardScaler
 
-from chemprop.models.model import MPNN
+from chemprop.models.model import MPNN, OutputTransform
 from chemprop.models.multi import MulticomponentMPNN
 
 def save_model(path: PathLike, model: MPNN, input_scalers: dict | None, output_scaler: StandardScaler | None) -> None:
@@ -26,6 +26,8 @@ def load_model(path: PathLike, multicomponent: bool) -> tuple[MPNN, dict | None,
     d = torch.load(path)
     input_scalers = d.get("input_scalers", None)
     output_scaler = d.get("output_scaler", None)
+    model.input_scalers = input_scalers
+    model.output_transform = OutputTransform(output_scaler) if output_scaler is not None else None
 
-    return model, input_scalers, output_scaler
+    return model
 
