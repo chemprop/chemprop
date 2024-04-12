02c760216ec03488b69b41abe184589b9c996d74
Kevin Greenman <kpg@mit.edu>
2024-04-12 13:30:57 -0400

    Bump version: 1.7.0 → 1.7.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, The Chemprop Development Team'
 author = 'The Chemprop Development Team'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.0'
+release = '1.7.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 # Load README
 with open("README.md", encoding="utf-8") as f:
