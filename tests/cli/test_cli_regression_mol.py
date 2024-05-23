d5f8d7a9b725fea458ace49e5830c4fb7284f214
Hao-Wei Pang <hwpang@mit.edu>
2024-05-23 13:05:46 -0400

    Test that we can train model using the config from hpopt

--- tests/cli/test_cli_regression_mol.py
+++ tests/cli/test_cli_regression_mol.py
@@ -422,3 +422,16 @@ def test_hyperopt_quick(monkeypatch, data_path, tmp_path):
     assert (tmp_path / "best_checkpoint.ckpt").exists()
     assert (tmp_path / "all_progress.csv").exists()
     assert (tmp_path / "ray_results").exists()
+
+    args = [
+        "chemprop",
+        "train",
+        "--config-path",
+        str(tmp_path / "best_config.toml"),
+    ]
+
+    with monkeypatch.context() as m:
+        m.setattr("sys.argv", args)
+        main()
+
+    assert (tmp_path / "model_0" / "best.pt").exists()
