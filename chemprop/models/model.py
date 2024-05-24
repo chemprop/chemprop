536052197bf83496f7bd8ff68a05c7aca00a3b9c
shihchengli <scli@mit.edu>
2024-05-24 16:56:30 -0400

    adding `sync_dist=True` into self.log

--- chemprop/models/model.py
+++ chemprop/models/model.py
@@ -158,7 +158,7 @@ class MPNN(pl.LightningModule):
         metric2loss = {f"val/{m.alias}": l for m, l in zip(self.metrics, losses)}
 
         self.log_dict(metric2loss, batch_size=len(batch[0]))
-        self.log("val_loss", losses[0], batch_size=len(batch[0]), prog_bar=True)
+        self.log("val_loss", losses[0], batch_size=len(batch[0]), prog_bar=True, sync_dist=True)
 
     def test_step(self, batch: TrainingBatch, batch_idx: int = 0):
         losses = self._evaluate_batch(batch)
