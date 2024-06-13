149ca3900e7e89fc56709007b46cb80910d37299
shihchengli <scli@mit.edu>
2024-06-13 12:24:16 -0400

    Add a template for uncertainty implementation

---
+++ chemprop/uncertainty/calibrator.py
@@ -0,0 +1,154 @@
+from abc import abstractmethod
+
+from torch import Tensor
+
+from chemprop.utils.registry import ClassRegistry
+
+
+class UncertaintyCalibrator:
+    @abstractmethod
+    def calibrate(self, preds: Tensor, uncs: Tensor, targets: Tensor, mask: Tensor) -> Tensor:
+        """
+        Fit calibration method for the calibration data.
+        """
+
+    @abstractmethod
+    def apply_calibration(self, preds: Tensor, uncs: Tensor, mask: Tensor) -> Tensor:
+        """
+        Take in predictions and uncertainty parameters from a model and apply the calibration method using fitted parameters.
+        """
+
+
+UncertaintyCalibratorRegistry = ClassRegistry[UncertaintyCalibrator]()
+
+
+@UncertaintyCalibratorRegistry.register("zscaling")
+class ZScalingCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("tscaling")
+class TScalingCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("zelikman-interval")
+class ZelikmanCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("mve-weighting")
+class MVEWeightingCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("platt")
+class PlattCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("conformal-multilabel")
+class ConformalMultilabelCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("conformal-multiclass")
+class ConformalMulticlassCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("conformal-adaptive")
+class ConformalAdaptiveMulticlassCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("conformal-regression")
+class ConformalRegressionCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("conformal-quantile-regression")
+class ConformalRegressionCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("isotonic")
+class IsotonicCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyCalibratorRegistry.register("isotonic-multiclass")
+class IsotonicMulticlassCalibrator(UncertaintyCalibrator):
+    def calibrate(self, preds, uncs, targets, mask) -> Tensor:
+        ...
+        return
+
+    def apply_calibration(self, preds, uncs, mask) -> Tensor:
+        ...
+        return
---
+++ chemprop/uncertainty/predictor.py
@@ -0,0 +1,108 @@
+from abc import abstractmethod
+from typing import Iterator
+
+from lightning import pytorch as pl
+from torch import Tensor
+
+from chemprop.data import DataLoader
+from chemprop.models.model import MPNN
+from chemprop.utils.registry import ClassRegistry
+
+
+class UncertaintyPredictor:
+    def __call__(self, dataloader: DataLoader, models: Iterator[MPNN], trainer: pl.Trainer):
+        return self._calc_prediction_uncertainty(dataloader, models, trainer)
+
+    @abstractmethod
+    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+        """
+        Calculate the uncalibrated predictions and uncertainties for the dataloader.
+        """
+        pass
+
+
+UncertaintyPredictorRegistry = ClassRegistry[UncertaintyPredictor]()
+
+
+@UncertaintyPredictorRegistry.register(None)
+class NoUncertaintyPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("mve")
+class MVEPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("ensemble")
+class EnsemblePredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("classification")
+class ClassPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("evidential-total")
+class EvidentialTotalPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("evidential-epistemic")
+class EvidentialEpistemicPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("evidential-aleatoric")
+class EvidentialAleatoricPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("dropout")
+class DropoutPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("spectra-roundrobin")
+class RoundRobinSpectraPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("dirichlet")
+class DirichletPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("conformal-quantile-regression")
+class ConformalQuantileRegressionPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
+
+
+@UncertaintyPredictorRegistry.register("conformal-regression")
+class ConformalRegressionPredictor(UncertaintyPredictor):
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
+        ...
+        return
