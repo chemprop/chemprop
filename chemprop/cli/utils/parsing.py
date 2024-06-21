d513e10dbe907df5629a598c164a0bfce234ef5d
Nathan Morgan <knathan@mit.edu>
2024-06-21 15:07:48 -0400

    clarify reactions have two mol objects

--- chemprop/cli/utils/parsing.py
+++ chemprop/cli/utils/parsing.py
@@ -164,7 +164,8 @@ def make_datapoints(
     molecule_featurizers : list[str] | None
         a list of molecule featurizer names to generate additional molecule features to use as extra
         descriptors. If there are multiple molecules per datapoint, the featurizers will be applied
-        to each molecule and concatenated. A reaction has two molecules, reactant and product.
+        to each molecule and concatenated. Note that a :code:`ReactionDatapoint` has two molecules,
+        reactant and product, so the molecule_featurizers will be applied to both.
     keep_h : bool
     add_h : bool
 
