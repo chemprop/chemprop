9d041d4df613cafeb06c6cb634e7e3853f98171b
Joel Manu <joelmanu@mit.edu>
2024-06-21 17:21:11 -0400

    draft for dropout and ensemble predictors

--- chemprop/uncertainty/predictor.py
+++ chemprop/uncertainty/predictor.py
@@ -1,8 +1,11 @@
 from abc import abstractmethod
 from typing import Iterator
 
+import numpy as np
+
 from lightning import pytorch as pl
 from torch import Tensor
+import torch.nn as nn
 
 from chemprop.data import DataLoader
 from chemprop.models.model import MPNN
@@ -40,11 +43,47 @@ class MVEPredictor(UncertaintyPredictor):
 
 @UncertaintyPredictorRegistry.register("ensemble")
 class EnsemblePredictor(UncertaintyPredictor):
+    """
+    Class that predicts uncertainty for predictions based on the variance in predictions among
+    an ensemble's submodels.
+    """
+    def __init__(self, individual_ensemble_predictions):
+        self.individual_ensemble_predictions = individual_ensemble_predictions
+
+
     def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
-        for model in models:
+        num_models = 0
+        for i, model in enumerate(models):
             preds = trainer.predict(model, dataloader)
-            print(preds.shape)
-        return
+            num_models += 1
+
+            if i == 0:
+                sum_preds = np.array(preds)
+                sum_squared = np.square(preds)
+
+                if self.individual_ensemble_predictions:
+                    individual_preds = np.expand_dims(np.array(preds), axis=-1)
+            else:
+                sum_preds += np.array(preds)
+                sum_squared += np.square(preds)
+
+                if self.individual_ensemble_predictions:
+                    individual_preds = np.append(
+                        individual_preds, np.expand_dims(preds, axis=-1), axis=-1
+                    )
+
+        uncal_preds = sum_preds / num_models
+        uncal_vars = (
+            sum_squared / num_models
+            - np.square(sum_preds) / num_models**2
+        )
+
+        final_preds = individual_preds if not self.individual_ensemble_predictions else uncal_preds
+
+        return (
+            final_preds.tolist(),
+            uncal_vars.tolist(),
+        )
 
 
 @UncertaintyPredictorRegistry.register("classification")
@@ -77,12 +116,46 @@ class EvidentialAleatoricPredictor(UncertaintyPredictor):
 
 @UncertaintyPredictorRegistry.register("dropout")
 class DropoutPredictor(UncertaintyPredictor):
+    """
+    Class that creates an artificial ensemble of models by applying monte carlo dropout to the loaded
+    model parameters. Predicts uncertainty for predictions based on the variance in predictions among
+    an ensemble's submodels.
+    """
+
+    def __init__(self, sampling_size, dropout_p):
+        self.sampling_size = sampling_size
+        self.dropout_p = dropout_p
+
     def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
-        assert num_models == 1, "Dropout method for uncertainty should be used for a single model rather than an ensemble."
-        
+        # TODO: uses first model if multiple are given, should throw error if multiple are given.
         model = next(models)
-        
-        return
+        model.apply(self.activate_dropout)
+
+        for i in range(self.sampling_size):
+            preds = trainer.predict(model, dataloader)
+            if i == 0:
+                sum_preds = np.array(preds)
+                sum_squared = np.square(preds)
+            else:
+                sum_preds += np.array(preds)
+                sum_squared += np.square(preds)
+
+        uncal_preds = sum_preds / self.sampling_size
+        uncal_vars = (
+            sum_squared / self.sampling_size
+            - np.square(sum_preds) / self.sampling_size**2
+        )
+
+        return (
+            uncal_preds.tolist(),
+            uncal_vars.tolist(),
+        )
+
+    # TODO: move to sensible location
+    def activate_dropout(self, module):
+        if isinstance(module, nn.Dropout):
+            module.p = self.dropout_p
+            module.train()
 
 
 @UncertaintyPredictorRegistry.register("spectra-roundrobin")
