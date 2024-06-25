88c23c4005eb925e09fa64ed5812eb9565a990c9
Hao-Wei Pang <hwpang@mit.edu>
2024-06-25 11:55:23 -0400

    Save target columns to .pt file

--- chemprop/models/utils.py
+++ chemprop/models/utils.py
@@ -6,8 +6,8 @@ from chemprop.models.model import MPNN
 from chemprop.models.multi import MulticomponentMPNN
 
 
-def save_model(path: PathLike, model: MPNN) -> None:
-    torch.save({"hyper_parameters": model.hparams, "state_dict": model.state_dict()}, path)
+def save_model(path: PathLike, model: MPNN, target_columns: list[str]=None) -> None:
+    torch.save({"hyper_parameters": model.hparams, "state_dict": model.state_dict(), "target_columns": target_columns}, path)
 
 
 def load_model(path: PathLike, multicomponent: bool) -> MPNN:
