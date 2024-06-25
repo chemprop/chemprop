b3e5b51d01b7e6c76ab1c1d22350aecca1719f3a
Hao-Wei Pang <hwpang@mit.edu>
2024-06-25 12:26:10 -0400

    Formatting

--- chemprop/cli/common.py
+++ chemprop/cli/common.py
@@ -28,11 +28,7 @@ def add_common_args(parser: ArgumentParser) -> ArgumentParser:
         action="store_true",
         help="If specified, the first row in the input CSV will not be used as column names.",
     )
-    data_args.add_argument(
-        "--ignore-columns",
-        nargs="+",
-        help="Name of the columns to ignore.",
-    )
+    data_args.add_argument("--ignore-columns", nargs="+", help="Name of the columns to ignore.")
 
     dataloader_args = parser.add_argument_group("Dataloader args")
     dataloader_args.add_argument(
