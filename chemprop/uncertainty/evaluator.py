f1ebbcc7f0e645ab59220e89675159d648de4e95
shihchengli <scli@mit.edu>
2024-08-09 12:02:56 -0400

    update the docstring for `UncertaintyEvaluator`

--- chemprop/uncertainty/evaluator.py
+++ chemprop/uncertainty/evaluator.py
@@ -14,6 +14,30 @@ class UncertaintyEvaluator(ABC):
     def evaluate(self, preds: Tensor, uncs: Tensor, targets: Tensor, mask: Tensor) -> Tensor:
         """
         Evaluate the performance of uncertainty predictions against the model target values.
+
+        **NOTE**: The `preds` is only needed for regression tasks. The `uncs` would be the predicted variance for regression tasks,
+        the predicted probability of class 1 for binary classification and the probabilities for each class for multiclass classification.
+
+        Parameters
+        ----------
+        preds: Tensor
+            the predictions for regression tasks. It is a tensor of the shape of ``n x t``, where ``n`` is the number of input
+            molecules/reactions, and ``t`` is the number of tasks.
+        uncs: Tensor
+            the predicted uncertainties of varying shape depending on the task type:
+
+            * regression/binary classification: ``n x t``
+
+            * multiclass classification: ``n x t x c``, where ``c`` is the number of classes
+        targets: Tensor
+            a tensor of the shape ``n x t``
+        mask: Tensor
+            a tensor of the shape ``n x t`` indicating whether the given values should be used in the fitting
+
+        Returns
+        -------
+        Tensor
+            a tensor of the shape ``t`` containing the evaluated metrics
         """
 
 
