602686ffa7dd1030b3437c0aa0eee17a836b9589
Hao-Wei Pang <hwpang@mit.edu>
2024-08-16 18:33:35 -0400

    Fix test

--- chemprop/nn/loss.py
+++ chemprop/nn/loss.py
@@ -229,7 +229,7 @@ class MulticlassMCCLoss(LossFunction):
         C = preds.shape[2]
         bin_targets = torch.eye(C, device=device)[targets]
         bin_preds = torch.eye(C, device=device)[preds.argmax(-1)]
-        masked_data_weights = weights * mask
+        masked_data_weights = weights.unsqueeze(2) * mask.unsqueeze(2)
 
         p = (bin_preds * masked_data_weights).sum(0)
         t = (bin_targets * masked_data_weights).sum(0)
