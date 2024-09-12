da974ee0384d52e827bd87bffef81d0d96ac010c
shihchengli <scli@mit.edu>
2024-09-11 21:42:37 -0400

    add integration test

--- tests/integration/test_classification_mol_multiclass.py
+++ tests/integration/test_classification_mol_multiclass.py
@@ -11,15 +11,6 @@ import torchmetrics
 from chemprop import nn
 from chemprop.data import MoleculeDatapoint, MoleculeDataset, collate_batch
 
-pytestmark = [
-    pytest.mark.parametrize(
-        "classification_mpnn_multiclass",
-        [nn.BondMessagePassing(), nn.AtomMessagePassing()],
-        indirect=True,
-    ),
-    pytest.mark.integration,
-]
-
 
 @pytest.fixture
 def data(mol_classification_data_multiclass):
@@ -35,6 +26,12 @@ def dataloader(data):
     return DataLoader(dset, 32, collate_fn=collate_batch)
 
 
+@pytest.mark.parametrize(
+    "classification_mpnn_multiclass",
+    [nn.BondMessagePassing(), nn.AtomMessagePassing()],
+    indirect=True,
+)
+@pytest.mark.integration
 def test_quick(classification_mpnn_multiclass, dataloader):
     trainer = pl.Trainer(
         logger=False,
@@ -48,6 +45,30 @@ def test_quick(classification_mpnn_multiclass, dataloader):
     trainer.fit(classification_mpnn_multiclass, dataloader, None)
 
 
+@pytest.mark.parametrize(
+    "classification_mpnn_multiclass_dirichlet",
+    [nn.BondMessagePassing(), nn.AtomMessagePassing()],
+    indirect=True,
+)
+@pytest.mark.integration
+def test_dirichlet_quick(classification_mpnn_multiclass_dirichlet, dataloader):
+    trainer = pl.Trainer(
+        logger=False,
+        enable_checkpointing=False,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        accelerator="cpu",
+        devices=1,
+        fast_dev_run=True,
+    )
+    trainer.fit(classification_mpnn_multiclass_dirichlet, dataloader, None)
+
+
+@pytest.mark.parametrize(
+    "classification_mpnn_multiclass",
+    [nn.BondMessagePassing(), nn.AtomMessagePassing()],
+    indirect=True,
+)
 def test_overfit(classification_mpnn_multiclass, dataloader):
     trainer = pl.Trainer(
         logger=False,
@@ -66,6 +87,41 @@ def test_overfit(classification_mpnn_multiclass, dataloader):
     for batch in dataloader:
         bmg, _, _, targets, *_ = batch
         preds = classification_mpnn_multiclass(bmg)
+        preds = preds.transpose(1, 2)
+        predss.append(preds)
+        targetss.append(targets)
+
+    preds = torch.cat(predss)
+    targets = torch.cat(targetss)
+    accuracy = torchmetrics.functional.accuracy(
+        preds, targets.long(), task="multiclass", num_classes=3
+    )
+    assert accuracy > 0.99
+
+
+@pytest.mark.parametrize(
+    "classification_mpnn_multiclass_dirichlet",
+    [nn.BondMessagePassing(), nn.AtomMessagePassing()],
+    indirect=True,
+)
+def test_dirichlet_overfit(classification_mpnn_multiclass_dirichlet, dataloader):
+    trainer = pl.Trainer(
+        logger=False,
+        enable_checkpointing=False,
+        enable_progress_bar=True,
+        enable_model_summary=False,
+        accelerator="cpu",
+        devices=1,
+        max_epochs=100,
+        overfit_batches=1.00,
+    )
+    trainer.fit(classification_mpnn_multiclass_dirichlet, dataloader)
+
+    predss = []
+    targetss = []
+    for batch in dataloader:
+        bmg, _, _, targets, *_ = batch
+        preds = classification_mpnn_multiclass_dirichlet(bmg)
         preds = preds.transpose(1, 2)
         predss.append(preds)
         targetss.append(targets)
