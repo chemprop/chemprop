8c6d637999fb1c54593fff8b92ab31797c0ae064
shihchengli <scli@mit.edu>
2024-10-20 01:47:02 -0400

    add unit test for mve and evidential models

--- tests/unit/uncertainty/test_predictors.py
+++ tests/unit/uncertainty/test_predictors.py
@@ -5,7 +5,14 @@ from torch.utils.data import DataLoader
 
 from chemprop.data import MoleculeDatapoint, MoleculeDataset, collate_batch
 from chemprop.models import MPNN
-from chemprop.uncertainty.predictor import DropoutPredictor, EnsemblePredictor
+from chemprop.uncertainty.predictor import (
+    DropoutPredictor,
+    EnsemblePredictor,
+    EvidentialAleatoricPredictor,
+    EvidentialEpistemicPredictor,
+    EvidentialTotalPredictor,
+    MVEPredictor,
+)
 
 
 @pytest.fixture
@@ -57,3 +64,44 @@ def test_EnsemblePredictor_wrong_n_models():
     predictor = EnsemblePredictor()
     with pytest.raises(ValueError):
         predictor("mock_dataloader", ["mock_model"], "mock_trainer")
+
+
+def test_MVEPredictor(data_dir, dataloader, trainer):
+    model = MPNN.load_from_file(data_dir / "example_model_v2_regression_mve_mol.pt")
+    predictor = MVEPredictor()
+    preds, uncs = predictor(dataloader, [model], trainer)
+
+    torch.testing.assert_close(preds, torch.tensor([[[2.10946], [2.10234]]]))
+    torch.testing.assert_close(uncs, torch.tensor([[[1.27602], [1.28058]]]))
+
+
+@pytest.mark.parametrize(
+    "predictor_class, expected_preds, expected_uncs",
+    [
+        (
+            EvidentialTotalPredictor,
+            torch.tensor([[[2.09985], [2.09525]]]),
+            torch.tensor([[[4.63703], [4.67548]]]),
+        ),
+        (
+            EvidentialEpistemicPredictor,
+            torch.tensor([[[2.09985], [2.09525]]]),
+            torch.tensor([[[2.77602], [2.80313]]]),
+        ),
+        (
+            EvidentialAleatoricPredictor,
+            torch.tensor([[[2.09985], [2.09525]]]),
+            torch.tensor([[[1.86101], [1.87234]]]),
+        ),
+    ],
+)
+def test_EvidentialPredictors(
+    predictor_class, expected_preds, expected_uncs, data_dir, dataloader, trainer
+):
+    model = MPNN.load_from_file(data_dir / "example_model_v2_regression_evidential_mol.pt")
+
+    predictor = predictor_class()
+    preds, uncs = predictor(dataloader, [model], trainer)
+
+    torch.testing.assert_close(preds, expected_preds)
+    torch.testing.assert_close(uncs, expected_uncs)
