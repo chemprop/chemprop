aec7522e1e21d0eabc865da84c10e6c8715748a2
brian li <twinbrian236@gmail.com>
2025-01-26 14:48:20 -0800

    parsings

---
+++ tests/cli/mixed_unit/test_parsing.py
@@ -0,0 +1,59 @@
+import pytest
+import pandas as pd
+from pathlib import Path
+
+from chemprop.cli.utils import build_mixed_data_from_files, make_dataset 
+
+from chemprop.data import MockDataset
+
+_DATA_PATH = Path(__file__).parent.parent.parent / "data/regression/mixed/mixed.csv"
+
+@pytest.fixture
+def data_path():
+    return _DATA_PATH
+
+@pytest.fixture
+def format_kwargs(data_path):
+    df = pd.read_csv(data_path)
+    format_kwargs = dict(
+        no_header_row=False,
+        smiles_cols=["smiles"],
+        rxn_cols=None,
+        ignore_cols=None,
+        splits_col=None,
+        weight_col=None,
+        bounded=False,
+    )
+    format_kwargs["target_cols"] = df.columns[1:].tolist()
+    return format_kwargs
+
+@pytest.fixture
+def data(data_path, format_kwargs):
+    featurization_kwargs = dict(
+        molecule_featurizers=None, keep_h=False, add_h=False
+    )
+    datas, mol_cols, atom_cols, bond_cols = build_mixed_data_from_files(
+        data_path,
+        **format_kwargs,
+        p_descriptors=None,
+        p_atom_feats=None,
+        p_bond_feats=None,
+        p_atom_descs=None,
+        **featurization_kwargs,
+    )
+    return [datas, mol_cols, atom_cols, bond_cols]
+
+@pytest.fixture
+def dataset(data):
+    dsets = [
+        make_dataset(datas[d], args.rxn_mode, args.multi_hot_atom_featurizer_mode, d)
+        if data[d + 1]
+        else MockDataset()
+        for d in range(len(data[0]))
+    ]
+    dset = data.MolAtomBondDataset(dsets[0], dsets[1], dsets[2])
+
+def test_data(data_path, data):
+    df = pd.read_csv(data_path)
+    assert len(data[1]) + len(data[2]) + len(data[3]) == df.shape[1] - 1
+    assert len(data[0]) == 3
