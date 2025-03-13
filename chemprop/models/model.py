73d9711e6b2939a5017b54a837673ba9dff3a3b9
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2025-03-13 13:17:21 -0400

    add a helpful warning when attempting to load 2.0 models in 2.1+

--- chemprop/models/model.py
+++ chemprop/models/model.py
@@ -253,7 +253,10 @@ class MPNN(pl.LightningModule):
 
     @classmethod
     def _load(cls, path, map_location, **submodules):
-        d = torch.load(path, map_location, weights_only=False)
+        try:
+            d = torch.load(path, map_location, weights_only=False)
+        except AttributeError:
+            logger.error(f"Model loading failed! It is possible this checkpoint was generated in v2.0 and needs to be converted to v2.1. Please run 'chemprop convert --conversion v2_0_to_v2_1 -i {path}' and load the converted checkpoint.")
 
         try:
             hparams = d["hyper_parameters"]
