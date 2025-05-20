154172dc13ee240b63bcebea8e56ce0768f6fab5
Nathan Morgan <knathan@mit.edu>
2025-05-20 14:34:32 -0400

    undo unintended v1 -> v2 hpopt serach space changes

--- chemprop/cli/hpopt.py
+++ chemprop/cli/hpopt.py
@@ -61,9 +61,9 @@ try:
     DEFAULT_SEARCH_SPACE = {
         "activation": tune.choice(categories=list(Activation.keys())),
         "aggregation": tune.choice(categories=list(AggregationRegistry.keys())),
-        "aggregation_norm": tune.quniform(lower=1, upper=200, q=1),
+        "aggregation_norm": tune.randint(lower=1, upper=200 + 1),
         "batch_size": tune.choice([16, 32, 64, 128, 256]),
-        "depth": tune.qrandint(lower=2, upper=6, q=1),
+        "depth": tune.randint(lower=2, upper=6 + 1),
         "dropout": tune.choice([0.0] * 8 + list(np.arange(0.05, 0.45, 0.05))),
         "ffn_hidden_dim": tune.qrandint(lower=300, upper=2400, q=100),
         "ffn_num_layers": tune.qrandint(lower=1, upper=3, q=1),
@@ -277,7 +277,7 @@ def build_search_space(search_parameters: list[str], train_epochs: int) -> dict:
         assert (
             train_epochs >= 6
         ), "Training epochs must be at least 6 to perform hyperparameter optimization for warmup_epochs."
-        SEARCH_SPACE["warmup_epochs"] = tune.qrandint(lower=1, upper=train_epochs // 2, q=1)
+        SEARCH_SPACE["warmup_epochs"] = tune.randint(lower=1, upper=train_epochs // 2 + 1)
 
     return {param: SEARCH_SPACE[param] for param in search_parameters}
 
