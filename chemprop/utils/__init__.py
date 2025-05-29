a570ba95b1b47da1153d7d83c2f7cf85ae8a1d34
Miha Skalic <7152667+miha-skalic@users.noreply.github.com>
2025-05-29 17:07:17 +0200

    Allow multiprocess loading.

--- chemprop/utils/__init__.py
+++ chemprop/utils/__init__.py
@@ -1,4 +1,11 @@
 from .registry import ClassRegistry, Factory
-from .utils import EnumMapping, make_mol, pretty_shape
+from .utils import EnumMapping, make_mol, parallel_execute, pretty_shape
 
-__all__ = ["ClassRegistry", "Factory", "EnumMapping", "make_mol", "pretty_shape"]
+__all__ = [
+    "ClassRegistry",
+    "Factory",
+    "EnumMapping",
+    "make_mol",
+    "pretty_shape",
+    "parallel_execute",
+]
