385bb2c1737fe8b96bb83d5aa3234ebaf3633fca
Jiaxu <jxl26@mit.edu>
2025-07-11 12:25:43 -0400

    revert unitentional change in unrelated file & minor fix of example

--- tests/integration/test_regression_MAB.py
+++ tests/integration/test_regression_MAB.py
@@ -52,7 +52,6 @@ def test_quick(mol_atom_bond_mpnn, dataloader):
     trainer.fit(mol_atom_bond_mpnn, dataloader, None)
 
 
-pl.seed_everything(0)
 @pytest.mark.parametrize(
     "mol_atom_bond_mpnn",
     [
