958384fa35b37ac4d32f93e762a466d42a4a0e46
JacksonBurns <jwburns@mit.edu>
2025-07-16 20:33:09 -0400

    fix circular import

--- chemprop/conf.py
+++ chemprop/conf.py
@@ -1,6 +1,4 @@
 """Global configuration variables for chemprop"""
 
-from chemprop.featurizers.molgraph.molecule import SimpleMoleculeMolGraphFeaturizer
-
-DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = SimpleMoleculeMolGraphFeaturizer().shape
+DEFAULT_ATOM_FDIM, DEFAULT_BOND_FDIM = 72, 14  # SimpleMoleculeMolGraphFeaturizer().shape
 DEFAULT_HIDDEN_DIM = 300
