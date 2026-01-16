63cf6c4f8cb6ce5d3487ca65ced5c9d62e934fa6
Nathan Morgan <knathan@mit.edu>
2026-01-16 16:05:22 -0500

    fix tests

--- tests/unit/nn/test_loss_functions.py
+++ tests/unit/nn/test_loss_functions.py
@@ -506,33 +506,33 @@ def test_Wasserstein(
     "preds,targets,mask,weights,n1,n2,expected_loss,method",
     [
         (
-            torch.tensor([[0, 2], [0, 1]], dtype=torch.float),  # preds
-            torch.zeros([2, 2], dtype=torch.float),  # targets
+            torch.tensor([[0.5], [0.5]], dtype=torch.float),  # preds
+            torch.tensor([[1, 2], [3, 4]], dtype=torch.int),  # targets
             torch.ones([2, 1], dtype=torch.bool),  # mask
             torch.ones([2]),  # weights
             torch.tensor(900, dtype=torch.int),  # n1
             torch.tensor(800, dtype=torch.int),  # n2
-            torch.tensor(1.68638, dtype=torch.float),  # exptected_loss
+            torch.tensor(0.21284, dtype=torch.float),  # exptected_loss
             "sqrt",
         ),
         (
-            torch.tensor([[0, 2], [0, 1]], dtype=torch.float),  # preds
-            torch.zeros([2, 2], dtype=torch.float),  # targets
+            torch.tensor([[0.5], [0.5]], dtype=torch.float),  # preds
+            torch.tensor([[1, 2], [3, 4]], dtype=torch.int),  # targets
             torch.ones([2, 2], dtype=torch.bool),  # mask
             torch.ones([2]),  # weights
             torch.tensor(500, dtype=torch.int),  # n1
             torch.tensor(600, dtype=torch.int),  # n2
-            torch.tensor(1.63835, dtype=torch.float),  # exptected_loss
+            torch.tensor(0.47931, dtype=torch.float),  # exptected_loss
             "score",
         ),
         (
-            torch.tensor([[0, 2], [0, 1]], dtype=torch.float),  # preds
-            torch.zeros([2, 2], dtype=torch.float),  # targets
+            torch.tensor([[0.5], [0.5]], dtype=torch.float),  # preds
+            torch.tensor([[1, 2], [3, 4]], dtype=torch.int),  # targets
             torch.ones([2, 2], dtype=torch.bool),  # mask
             torch.ones([2]),  # weights
             torch.tensor(10, dtype=torch.int),  # n1
             torch.tensor(1000, dtype=torch.int),  # n2
-            torch.tensor(2.7664, dtype=torch.float),  # exptected_loss
+            torch.tensor(1.79683, dtype=torch.float),  # exptected_loss
             "wald",
         ),
     ],
