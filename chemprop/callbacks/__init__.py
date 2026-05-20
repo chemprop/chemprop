06666d33b585a7e837e634974c00062390585900
s-homberg <samuel.homberg@uni-muenster.de>
2026-05-20 20:11:13 +0200

    Undo flake8 E402, to avoid circular export
    ImportError: cannot import name 'CallbackRegistry' from partially initialized module 'chemprop.callbacks' (most likely due to a circular import) (chemprop/chemprop/callbacks/__init__.py)

--- chemprop/callbacks/__init__.py
+++ chemprop/callbacks/__init__.py
@@ -1,8 +1,9 @@
 from lightning.pytorch.callbacks import Callback
 from chemprop.utils.registry import ClassRegistry
-from .example_callbacks import ExampleCallback
-from .interpret_callbacks import MyersonExplainerCallback
 
 CallbackRegistry = ClassRegistry[Callback]()
+
+from .example_callbacks import ExampleCallback
+from .interpret_callbacks import MyersonExplainerCallback
 
 __all__ = ["CallbackRegistry", "ExampleCallback", "MyersonExplainerCallback"]
