ac2f87d493f39442c6dfaf474f0c8ae76e21a245
Nathan Morgan <knathan@mit.edu>
2026-08-03 15:12:59 -0400

    add test for not mutating input

--- tests/integration/test_regression_mol.py
+++ tests/integration/test_regression_mol.py
@@ -7,7 +7,7 @@ import pytest
 import torch
 from torch.utils.data import DataLoader
 
-from chemprop import nn
+from chemprop import models, nn
 from chemprop.data import MoleculeDatapoint, MoleculeDataset, collate_batch
 
 
@@ -135,3 +135,32 @@ def test_quantile_quick(regression_mpnn_quantile, dataloader):
         fast_dev_run=True,
     )
     trainer.fit(regression_mpnn_quantile, dataloader, None)
+
+
+def test_input_not_mutated(dataloader):
+    V_f_transform = nn.ScaleTransform(mean=[1.0, 2.0], scale=[3.0, 4.0], pad=70)
+    E_f_transform = nn.ScaleTransform(mean=[0.0, 1.0], scale=[2.0, 3.0], pad=12)
+    graph_transform = nn.GraphTransform(V_transform=V_f_transform, E_transform=E_f_transform)
+    V_d_transform = nn.ScaleTransform(mean=[4.0, 5.0], scale=[6.0, 7.0])
+    mp = nn.BondMessagePassing(graph_transform=graph_transform, V_d_transform=V_d_transform)
+
+    output_transform = nn.UnscaleTransform(mean=[8.0, 9.0], scale=[10.0, 11.0])
+    ffn = nn.RegressionFFN(output_transform=output_transform)
+
+    X_d_transform = nn.ScaleTransform(mean=[13.0, 14.0], scale=[15.0, 16.0])
+    model = models.MPNN(mp, nn.NormAggregation(), ffn, X_d_transform=X_d_transform)
+
+    batch = next(iter(dataloader))
+    bmg, *_ = batch
+
+    batch2 = next(iter(dataloader))
+    bmg2, *_ = batch2
+
+    model.eval()
+    _ = model(bmg)
+
+    assert torch.allclose(bmg.V, bmg2.V)
+    assert torch.allclose(bmg.E, bmg2.E)
+    assert torch.allclose(bmg.edge_index, bmg2.edge_index)
+    assert torch.allclose(bmg.rev_edge_index, bmg2.rev_edge_index)
+    assert torch.allclose(bmg.batch, bmg2.batch)
